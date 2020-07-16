d7b5d037544035905c428525304e7879b25a2f00
성수 박 <whiteibescu@gmail.com>
2020-07-16 18:05:26 +0900

    수업

---
+++ _10_MoveEllipseTimer/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _10_MoveEllipseTimer
+{
+    public partial class Form1 : Form
+    {
+        const int RAD = 100;
+        const int MOVE_VAL = 20;
+        int x = 0, y = 0;
+        Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.KeyDown += Form1_KeyDown;
+            timer.Interval = 100;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode)
+            {
+                case Keys.Enter:
+                    timer.Start();
+                    break;
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen myPen = new Pen(Brushes.DarkBlue, 5);
+            g.DrawEllipse(myPen, x, y, RAD, RAD);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            this.x += MOVE_VAL;
+            Invalidate();
+        }
+    }
+}
