01909bdb4631c5aef72b6075af4e32ec7caea2d4
성수 박 <whiteibescu@gmail.com>
2020-07-22 00:47:29 +0900

    커밋

---
+++ _2020_07_21Assignment/Problem03/TrafficLight.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Problem03
+{
+    public partial class TrafficLight : Form
+    {
+        const int DIA = 100;
+        int x = 10;
+        int y = 10;
+        int width = DIA;
+        int height = DIA;
+        Timer timer = new Timer();
+        int cycle = 0;
+        Brush color = Brushes.Red;
+
+        Rectangle[] rectangles = new Rectangle[3];
+        public TrafficLight()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            timer.Interval = 1000;
+            timer.Tick += Timer_Tick;
+            this.Paint += TrafficLight_Paint;
+            timer.Start();
+        }
+
+        private void TrafficLight_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Pen pen = new Pen(Brushes.White);
+            rectangles[0] = new Rectangle(x, y, DIA, DIA);
+            for (int i = 0; i < 3; i++)
+            {
+                rectangles[i] = new Rectangle(x, y + i * (width + 10), width, height);
+                g.DrawEllipse(pen, rectangles[i]);
+            }
+
+            g.FillEllipse(color, rectangles[cycle]);
+            cycle = ++cycle % 3;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (cycle == 0)
+            {
+                color = Brushes.Red;
+            }
+            else if (cycle == 1)
+            {
+                color = Brushes.Yellow;
+            }
+            else if (cycle == 2)
+
+            {
+                color = Brushes.Green;
+            }
+
+            Invalidate();
+        }
+    }
+}
