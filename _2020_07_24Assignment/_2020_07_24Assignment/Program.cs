f83e3b876e9fe08c6996a58f66739e96c11c28d5
성수 박 <whiteibescu@gmail.com>
2020-07-25 22:15:56 +0900

    commit

---
+++ _2020_07_24Assignment/_2020_07_24Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _2020_07_24Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("영문자를 입력해주세요 : ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.Any(char.IsUpper) ? "Yes" : "No");
+
+
+/*            if (input.Any(char.IsUpper))
+            {
+                Console.WriteLine(input);
+            }
+            else
+            {
+                Console.WriteLine("소문자입니다 :" + input);
+            }*/
+        }
+    }
+}
