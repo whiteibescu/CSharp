ab3ab7c5198debaa0c70409fdb616930fc551c2e
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:46:01 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem07/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _2020_07_24Problem07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("x좌표를 입력하세요 >> ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("y좌표를 입력하세요 >> ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            if (x >= 50 && x <= 100 && y >= 50 && y <= 100)
+            {
+                Console.WriteLine("겹침");
+            }
+            else
+            {
+                Console.WriteLine("안겹침");
+            }
+        }
+    }
+}
