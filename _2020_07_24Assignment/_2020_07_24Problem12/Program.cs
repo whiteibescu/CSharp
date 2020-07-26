0a0d66cceda8755903cf129fd50283cd984a498f
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:46:27 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem12/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2020_07_24Problem12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 1;
+
+            Console.WriteLine("1부터 10까지 곱해서 결과를 출력합니다 : ");
+            
+            for (int i =2; i < 11; ++i )
+            {
+                sum *= i;
+                Console.WriteLine("i : " + i + "sum : " + sum);
+            }
+        }
+    }
+}
