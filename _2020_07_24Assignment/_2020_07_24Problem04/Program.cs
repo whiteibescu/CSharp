2721bb696c5e19d7912ca64208d53b916a560a7f
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:45:44 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020_07_24Problem04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
