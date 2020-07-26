97511f9c045df2440efbf34132813f18de5aa22d
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:46:13 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem09/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _2020_07_24Problem09
+{
+    /* 9.int배열을 10개 생성해라
+  사용자로부터 10개의 값을 입력받고
+  순서대로 출력하고 
+  총합을 구하세요 */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            int sum = 0;
+
+            Console.WriteLine("정수 10개 입력하세요 : ");
+
+            for (int i = 0; i < 10; ++i)
+            {
+                string input = Console.ReadLine();
+                int num = Convert.ToInt32(input);
+                arr[i] = num;
+                sum += arr[i]; 
+            }
+
+            Console.WriteLine("입력된 10개의 정수 =>> ");
+            for (int i = 0; i < 10; ++i)
+            {
+            Console.Write(arr[i] + " ");
+            }
+            Console.WriteLine("\n총합 : " + sum);
+        }
+    }
+}
