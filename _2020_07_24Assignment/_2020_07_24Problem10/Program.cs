f6d5e97cfc73ea3b1cc7464f21c56b34f5cbc39c
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:46:18 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2020_07_24Problem10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            Random random = new Random();
+
+            for (int i = 0; i < 10; ++i)
+            {
+                arr[i] = random.Next(10);
+
+                Console.WriteLine("10개의 난수생성: " + arr[i]);
+                
+                if (arr[i] % 2 != 0 || arr[i] == 1)
+                {
+                    Console.WriteLine("홀수 :" + arr[i]);
+                }
+            }
+        }
+    }
+}
