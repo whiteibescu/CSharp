0ae4fab660094bc3b22ec6bf61c52360ac84b214
성수 박 <whiteibescu@gmail.com>
2020-07-26 22:45:34 +0900

    과제

---
+++ _2020_07_24Assignment/_2020_07_24Problem02/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _2020_07_24Problem02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("영문자를 입력해주세요 : ");
+            string input = Console.ReadLine();
+
+            if (65 <= input & input <= 90)
+            {
+
+                input += 32;
+                Console.WriteLine(input);
+
+            }
+            else if (97 <= input & input <= 122)
+            {
+
+                input -= 32;
+                Console.WriteLine(input);
+
+
+
+            }
+        }
+    }
+}
