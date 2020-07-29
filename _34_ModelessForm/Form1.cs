cadb5f1c7308d5fa8e023ce82fbe63e192a0bf81
성수 박 <whiteibescu@gmail.com>
2020-07-29 14:30:56 +0900

    Lecture

---
+++ _34_ModelessForm/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _34_ModelessForm
+{
+    public partial class Form1 : Form
+    {
+        MyForm myForm = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnModelessForm_Click(object sender, EventArgs e)
+        {
+            if(myForm==null || myForm.IsDisposed)
+            {
+                myForm = new MyForm();
+                myForm.Show();
+            }
+        }
+    }
+}
