6bff70426683970c2d69446390ec646875be9ab9
성수 박 <whiteibescu@gmail.com>
2020-07-29 14:31:04 +0900

    Lecture

---
+++ _35_MDIForm/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _35_MDIForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.IsMdiContainer = true;
+        }
+
+        private void 새파일ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm childForm = new ChildForm();
+            childForm.MdiParent = this;
+            childForm.Show();
+        }
+
+        private void 닫기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form childForm = this.ActiveMdiChild;
+            if (childForm != null)
+                childForm.Close();
+        }
+
+        private void 계단식정렬ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void 수평바둑판정렬ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void 수직바둑판정렬ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+    }
+}
