858ffcddd7b3fb3bd7409e92817e644105233fd6
성수 박 <whiteibescu@gmail.com>
2020-07-29 14:30:37 +0900

    Lecture

---
+++ _32_ModalForm/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace _32_ModalForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnModalForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnModalForm
+            // 
+            this.btnModalForm.Location = new System.Drawing.Point(253, 129);
+            this.btnModalForm.Name = "btnModalForm";
+            this.btnModalForm.Size = new System.Drawing.Size(386, 108);
+            this.btnModalForm.TabIndex = 0;
+            this.btnModalForm.Text = "모달 폼 띄우기";
+            this.btnModalForm.UseVisualStyleBackColor = true;
+            this.btnModalForm.Click += new System.EventHandler(this.btnModalForm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnModalForm);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnModalForm;
+    }
+}
+
---
+++ _32_ModalForm/MyForm.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace _32_ModalForm
+{
+    partial class MyForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MyForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.BackgroundImage = global::_32_ModalForm.Properties.Resources.고해상도에펠탑아래서;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "MyForm";
+            this.Text = "MyForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
