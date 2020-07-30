2adf753da82496ec579b5cbf66e66bcbe920597d
성수 박 <whiteibescu@gmail.com>
2020-07-30 13:50:26 +0900

    수업

---
+++ _2020_07_30Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2020_07_30Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
