b13aac51157b69e1f0e76066a99aaffcd96c8fda
성수 박 <whiteibescu@gmail.com>
2020-08-02 22:49:57 +0900

    과제

---
+++ _2020_07_31Assignment/_2020_07_31Assignment/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_07_31Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "1";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 10; ++id)
+        }
+
+        
+    }
+}
