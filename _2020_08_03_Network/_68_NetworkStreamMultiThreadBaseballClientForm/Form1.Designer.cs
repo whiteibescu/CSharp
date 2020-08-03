3f9f2bafbfc61ddd02b61c9e33f71958b67538b4
성수 박 <whiteibescu@gmail.com>
2020-08-03 14:01:06 +0900

    Lecture

---
+++ _2020_08_03_Network/_68_NetworkStreamMultiThreadBaseballClientForm/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace _64_NetworkStreamMultiThreadClientForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbIp = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.lbLog = new System.Windows.Forms.ListBox();
+            this.tbChat = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnBaseBallRun = new System.Windows.Forms.Button();
+            this.tbNum2 = new System.Windows.Forms.TextBox();
+            this.tbNum1 = new System.Windows.Forms.TextBox();
+            this.tbNum0 = new System.Windows.Forms.TextBox();
+            this.btnBaseBallStart = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(27, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(27, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(182, 38);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Server Port";
+            // 
+            // tbIp
+            // 
+            this.tbIp.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbIp.Location = new System.Drawing.Point(227, 18);
+            this.tbIp.Name = "tbIp";
+            this.tbIp.Size = new System.Drawing.Size(227, 45);
+            this.tbIp.TabIndex = 1;
+            this.tbIp.Text = "127.0.0.1";
+            this.tbIp.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // tbPort
+            // 
+            this.tbPort.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbPort.Location = new System.Drawing.Point(227, 91);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(227, 45);
+            this.tbPort.TabIndex = 1;
+            this.tbPort.Text = "9000";
+            this.tbPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnConnect.Location = new System.Drawing.Point(477, 20);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(214, 55);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDisconnect.Location = new System.Drawing.Point(477, 81);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(214, 55);
+            this.btnDisconnect.TabIndex = 2;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClear.Location = new System.Drawing.Point(714, 81);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(161, 55);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // lbLog
+            // 
+            this.lbLog.Font = new System.Drawing.Font("휴먼편지체", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lbLog.FormattingEnabled = true;
+            this.lbLog.ItemHeight = 31;
+            this.lbLog.Location = new System.Drawing.Point(20, 153);
+            this.lbLog.Name = "lbLog";
+            this.lbLog.Size = new System.Drawing.Size(855, 407);
+            this.lbLog.TabIndex = 3;
+            // 
+            // tbChat
+            // 
+            this.tbChat.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbChat.Location = new System.Drawing.Point(37, 57);
+            this.tbChat.Name = "tbChat";
+            this.tbChat.Size = new System.Drawing.Size(705, 45);
+            this.tbChat.TabIndex = 4;
+            this.tbChat.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbChat_KeyDown);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tbChat);
+            this.groupBox1.Font = new System.Drawing.Font("휴먼편지체", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox1.Location = new System.Drawing.Point(23, 569);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(851, 139);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Chatting";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnBaseBallRun);
+            this.groupBox2.Controls.Add(this.tbNum2);
+            this.groupBox2.Controls.Add(this.tbNum1);
+            this.groupBox2.Controls.Add(this.tbNum0);
+            this.groupBox2.Controls.Add(this.btnBaseBallStart);
+            this.groupBox2.Font = new System.Drawing.Font("휴먼편지체", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.groupBox2.Location = new System.Drawing.Point(23, 714);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(851, 159);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BaseBall";
+            // 
+            // btnBaseBallRun
+            // 
+            this.btnBaseBallRun.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnBaseBallRun.Location = new System.Drawing.Point(680, 66);
+            this.btnBaseBallRun.Name = "btnBaseBallRun";
+            this.btnBaseBallRun.Size = new System.Drawing.Size(154, 52);
+            this.btnBaseBallRun.TabIndex = 2;
+            this.btnBaseBallRun.Text = "전송";
+            this.btnBaseBallRun.UseVisualStyleBackColor = true;
+            this.btnBaseBallRun.Click += new System.EventHandler(this.btnBaseBallRun_Click);
+            // 
+            // tbNum2
+            // 
+            this.tbNum2.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbNum2.Location = new System.Drawing.Point(574, 70);
+            this.tbNum2.Name = "tbNum2";
+            this.tbNum2.Size = new System.Drawing.Size(94, 45);
+            this.tbNum2.TabIndex = 1;
+            // 
+            // tbNum1
+            // 
+            this.tbNum1.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbNum1.Location = new System.Drawing.Point(474, 70);
+            this.tbNum1.Name = "tbNum1";
+            this.tbNum1.Size = new System.Drawing.Size(94, 45);
+            this.tbNum1.TabIndex = 1;
+            // 
+            // tbNum0
+            // 
+            this.tbNum0.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.tbNum0.Location = new System.Drawing.Point(374, 70);
+            this.tbNum0.Name = "tbNum0";
+            this.tbNum0.Size = new System.Drawing.Size(94, 45);
+            this.tbNum0.TabIndex = 1;
+            // 
+            // btnBaseBallStart
+            // 
+            this.btnBaseBallStart.Font = new System.Drawing.Font("휴먼편지체", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnBaseBallStart.Location = new System.Drawing.Point(11, 70);
+            this.btnBaseBallStart.Name = "btnBaseBallStart";
+            this.btnBaseBallStart.Size = new System.Drawing.Size(262, 49);
+            this.btnBaseBallStart.TabIndex = 0;
+            this.btnBaseBallStart.Text = "시작 / 다시 시작";
+            this.btnBaseBallStart.UseVisualStyleBackColor = true;
+            this.btnBaseBallStart.Click += new System.EventHandler(this.btnBaseBallStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(887, 885);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lbLog);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbIp);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbIp;
+        private System.Windows.Forms.TextBox tbPort;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lbLog;
+        private System.Windows.Forms.TextBox tbChat;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnBaseBallRun;
+        private System.Windows.Forms.TextBox tbNum2;
+        private System.Windows.Forms.TextBox tbNum1;
+        private System.Windows.Forms.TextBox tbNum0;
+        private System.Windows.Forms.Button btnBaseBallStart;
+    }
+}
+
