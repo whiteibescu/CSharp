80cd13a05991cadc53589cab8bf979ba7febc71c
성수 박 <whiteibescu@gmail.com>
2020-08-04 08:44:34 +0900

    과제

---
+++ _2020_08_03TryCatch/_2020_08_03TryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _2020_08_03TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MaxValue, y = 0;
+                //int num;
+                //Console.WriteLine("정수를 입력하세요");
+                //num = Int32.Parse(Console.ReadLine());
+            
+            try
+            {
+                //Console.WriteLine(x);
+                y = x + 10;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }           
+        }
+    }
+}
