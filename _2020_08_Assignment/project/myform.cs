bf4c34aa7ebf926b33459dbeeb4c0cdb97c3dbdd
성수 박 <whiteibescu@gmail.com>
2020-08-04 22:56:50 +0900

    commit

---
+++ _2020_08_Assignment/project/myform.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class myform : Form
+    {
+        public myform()
+        {
+            InitializeComponent();
+            this.Layout += Myform_Layout;
+        }
+
+        private void Myform_Layout(object sender, LayoutEventArgs e)
+        {
+            this.axWindowsMediaPlayer1.Size = this.ClientSize;
+
+        }
+    }
+}
