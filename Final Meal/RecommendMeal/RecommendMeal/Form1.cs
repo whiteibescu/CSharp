af384dd17e4d3181c14f1d0fe8a2672169686eb0
성수 박 <whiteibescu@gmail.com>
2020-08-05 17:04:23 +0900

    Assignment

---
+++ Final Meal/RecommendMeal/RecommendMeal/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RecommendMeal
+{
+
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnModalForm_Click(object sender, EventArgs e)
+        {
+/*            CookingForm cookingForm = new CookingForm();
+
+            cookingForm.ShowDialog();*/
+
+
+           /* CookingForm cookingForm = new CookingForm(this);
+            cookingForm.Show();*/
+            LoadingForm loadingForm = new LoadingForm();
+            loadingForm.Show();
+             
+
+        }
+
+        private void btnEggs_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("계란");
+            btnEggs.Hide();
+        }
+
+        private void btnTuna_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("참치");
+            btnTuna.Hide();
+        }
+
+        private void btnOnion_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("양파");
+            btnOnion.Hide();
+        }
+
+        private void btnKimchi_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("김치");
+            btnKimchi.Hide();
+        }
+
+        private void btnTofu_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("두부");
+            btnTofu.Hide();
+        }
+
+        private void btnRice_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("냉동밥");
+            btnRice.Hide();
+        }
+
+        private void btnLeftMove_Click(object sender, EventArgs e)
+        {
+            /*listBox1.SelectedIndex*/
+
+            try
+            {
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("제거할 재료를 선택하세요!");
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            StartingForm sf = new StartingForm();
+            sf.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Final Meal/RecommendMeal/RecommendMeal/LoadingForm.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace RecommendMeal
+{
+    public partial class LoadingForm : Form
+    {
+        Image image = null;
+        Timer FormTimer = new Timer();
+        Timer progressbarTimer = new Timer();
+
+        int i = 0;
+        int num = 20;
+        int x = 300, y = 100;
+        public LoadingForm()
+        {
+            InitializeComponent();
+            this.Paint += Form1_Paint;
+            this.Load += Form1_Load;
+            FormTimer.Interval = 1000;
+            FormTimer.Tick += Timer_Tick;
+            FormTimer.Start();
+            progressbarTimer.Interval = 1000;
+            progressbarTimer.Tick += Progressbar_Tick;
+            progressbarTimer.Start();
+            //this.DoubleBuffered = true;
+        }
+
+        private void Progressbar_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += num;
+            if (progressBar1.Value == 100)
+            {
+                progressbarTimer.Stop();
+               
+            }
+
+
+
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            i++;
+            if (i == 6) { this.Close(); }
+            
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            image = Image.FromFile("../../Cooking.gif");
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
