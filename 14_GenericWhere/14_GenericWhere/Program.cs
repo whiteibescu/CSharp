fcc53632ac3890f770fef47c4166fd1bec9ec603
성수 박 <whiteibescu@gmail.com>
2020-08-25 09:25:26 +0900

    Generic Where

---
+++ 14_GenericWhere/14_GenericWhere/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _14_GenericWhere
+{
+    class Wrapper<T> where T : struct
+    {
+        T Value;
+        public Wrapper() { Value = default(T); }
+        public Wrapper(T aValue)
+        { Value = aValue; }
+        public T Data
+        {
+            get { return Value; }
+            set { Value = value; }
+        }
+        public void OutValue()
+        {
+            Console.WriteLine(Value);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wrapper<int> gi =
+                new Wrapper<string>(1234);
+            gi.OutValue();
+        }
+    }
+}
