475f69a5b132f2bd472d41963b6f53573ac2a3af
SungSoo Park <whiteibescu@gmail.com>
2020-09-15 08:45:41 +0900

    lecture

---
+++ MergeSort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MergeSort
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 9, 8, 5, 4, 2, 3, 7, 6 };
+
+            for (int i = 0; i< array.Length; ++i)
+            {
+                Console.WriteLine(array[i] + " ");
+            }
+            Console.WriteLine();
+
+            mergeSort(array, 0, array.Length - 1);
+        }
+    }
+
+    public static void mergeSort(int[] src, int from, int to)
+    {
+        int[] temp = new int[src.Length];
+
+        if (from < to)
+        {
+            int mid = (from + to);
+        }
+
+     public static void MergeSort()
+            {
+    }
+}
---
+++ ViewForm/Image2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ViewForm
+{
+    public partial class Image2 : Form
+    {
+        public Image2()
+        {
+            InitializeComponent();
+        }
+    }
+}
