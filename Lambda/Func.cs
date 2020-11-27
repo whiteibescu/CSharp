a3587a1cee39f3e05be180b91139105d49a32348
SungSoo Park <whiteibescu@gmail.com>
2020-11-27 16:14:15 +0900

    Func

---
+++ Lambda/Func.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lambda
+{
+    class Func
+    {
+        static void Main(string[] args)
+        {
+            Func<int> func1 = () => 123;
+            Console.WriteLine("func1의 값 : {0}", func1());
+            Func<int, int> func2 = (x) => x * 2;
+            Console.WriteLine("func2의 값: {0}", func2(5));
+        }
+
+    }
+}
