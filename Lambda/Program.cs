3c4b1e7b05746e59588c0f7631c6c87606aea3fb
SungSoo Park <whiteibescu@gmail.com>
2020-11-27 16:09:37 +0900

    Lambda

---
+++ Lambda/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lambda
+{
+    class Program
+    {
+        delegate void Calculate(int a, int b);
+        static void Main()
+        {
+            Calculate cal = (int a, int b) =>
+            {
+                int sum = a + b;
+                Console.WriteLine(sum);
+            };
+
+            cal(10, 20);
+        }
+    }
+}
