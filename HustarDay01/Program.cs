dffadd20f2e2ac73b5041c249110ff8622261058
SungSoo Park <whiteibescu@gmail.com>
2021-05-03 11:16:40 +0900

    Hustar CSharp Lecture Day 01

---
+++ HustarDay01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace HustarDay01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            int z;
+
+            x = 10;
+            y = 20;
+            z = x + y;
+
+            Console.WriteLine("{0} + {1} = {2}", x,y,z);
+        }
+    }
+}
