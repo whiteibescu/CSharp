17428a3e4ec810e16eb5882aeddb13da496a71ef
SungSoo Park <whiteibescu@gmail.com>
2021-05-04 15:30:39 +0900

    Call By Value

---
+++ HustarDay02/Important.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay02
+{
+    class Important
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Console.WriteLine("x:{0},y:{1}", x, y);
+            MySwap(x, y);
+            Console.WriteLine("x:{0},y:{1}", x, y);
+        }
+
+        static void MySwap(int px, int py)
+        {
+            int temp = px;
+            px = py;
+            py = temp;
+        }
+    }
+}
