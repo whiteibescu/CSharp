35c8d9cea36add85f8ebb9b69cdaf49f01095e1e
SungSoo Park <whiteibescu@gmail.com>
2021-05-04 15:30:27 +0900

    DrawThis

---
+++ HustarDay02/DrawThis.cs
@@ -0,0 +1,31 @@
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    namespace HustarDay02
+    {
+        class Cat
+        {
+            public int m_iAge;
+        }
+        class DrawThis
+        {
+            static void Main(string[] args)
+            {
+                Cat c1 = new Cat();
+                c1.m_iAge = 10;
+                Cat c2 = new Cat();
+                c2.m_iAge = 20;
+                Console.WriteLine("c1.m_iAge : {0},c2.m_iAge : {1}", c1.m_iAge, c2.m_iAge);
+                MySwap(c1, c2);
+                Console.WriteLine("c1.m_iAge : {0},c2.m_iAge : {1}", c1.m_iAge, c2.m_iAge);
+            }
+
+            static void MySwap(Cat pc1, Cat pc2)
+            {
+                Cat temp = pc1;
+                pc1 = pc2;
+                pc2 = temp;
+            }
+        }
+    }
