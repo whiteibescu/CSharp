84d143b2869ad8aebac36103bea6148c49ffb4c0
SungSoo Park <whiteibescu@gmail.com>
2021-05-06 11:59:31 +0900

    SystemObject

---
+++ HustarDay03/SystemObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay03
+{
+    class Cat
+    {
+        public int m_iAge;
+    }
+
+    class SystemObject
+    {
+        static void main(string[] args)
+        {            
+            int i = 10;
+            object oi = i;
+            Console.WriteLine("{0}", i.ToString());
+            Cat c1 = new Cat();
+            object oc = c1;
+            Console.WriteLine("{0}", c1.ToString());
+        }
+    }
+}
