b3c15edfbcb6250426fcd64d462c5d607e5d6b25
SungSoo Park <whiteibescu@gmail.com>
2021-05-06 11:58:25 +0900

    StackandHeap02

---
+++ HustarDay03/StackandHeap02.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay03
+{
+    class StackandHeap02
+    {
+        class Cat
+        {
+            public string name;
+        }
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat();
+            Console.WriteLine("c1.name:{0},", c1.name);
+            c1.name = "야옹이";
+            Console.WriteLine("c1.name:{0},", c1.name);
+        }
+    }
+}
