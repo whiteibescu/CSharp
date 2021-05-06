6ab51e24fb59f4e01c8c0c6dceeb250f8c5ba500
SungSoo Park <whiteibescu@gmail.com>
2021-05-06 13:46:19 +0900

    SystemObject02

---
+++ HustarDay03/SystemObject02.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay03
+{
+    class SystemObject02
+    {
+        class Cat
+        {
+            public override bool Equals(object obj)
+            {
+                return base.Equals(obj);
+            }
+            public override string ToString()
+            {
+                return "m_Name :" + m_Name;
+            }
+            public string GetName()
+            {
+                return m_Name;
+            }
+            public void SetAge(string Name)
+            {
+                m_Name = Name;
+            }
+            private string m_Name;
+        }
+
+        static void Main(string[] args)
+        {
+            Cat c1 = new Cat();
+            c1.SetAge("홍길동");
+            Cat c2 = new Cat();
+            c2.SetAge("홍길동");
+
+            if (c1.Equals(c2))
+            {
+                Console.WriteLine("c1 == c2");
+            }
+            else
+            {
+                Console.WriteLine("c1 != c2");
+            }
+        }
+    }
+}
