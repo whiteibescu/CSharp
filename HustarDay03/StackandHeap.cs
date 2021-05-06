bf9504f2b8578dd35db11fa493bd3cd33d20b4f6
SungSoo Park <whiteibescu@gmail.com>
2021-05-06 11:58:17 +0900

    StackandHeap

---
+++ HustarDay03/StackandHeap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "홍길동";
+            Console.WriteLine("s1 : {0}", s1);
+            MyFunc(s1);
+            Console.WriteLine("s1 : {0}", s1);
+        }
+        static void MyFunc(string s)
+        {
+            s = "김길동";
+            Console.WriteLine("s : {0}", s);
+        }
+    }
+}
