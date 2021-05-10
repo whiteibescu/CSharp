6c5539254b5b8bf00f4242429277ff396595046d
SungSoo Park <whiteibescu@gmail.com>
2021-05-10 13:41:22 +0900

    enumerator

---
+++ HustarDay05/Enumerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay05
+{
+    enum COLOR
+    {
+        RED=100, GREEN, BLUE
+    }
+    class Enumerator
+    {
+        COLOR r = COLOR.RED;
+        Console.WriteLine(r);
+        Console.WriteLine((int)r);
+
+            
+    }
+}
