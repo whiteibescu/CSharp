cd33eb723152a50f6e22add98a63d5e0bcfca8a5
SungSoo Park <whiteibescu@gmail.com>
2021-05-10 13:41:01 +0900

    array

---
+++ HustarDay05/Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HustarDay05
+{
+    class Array
+    {
+        static void Main(string[] args)
+        {
+            int[] iArray = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(iArray)
+            for (int i = 0 i < iArray.Length; ++i)
+            {
+                Console.WriteLine("iArray[{0}] = {1}", i, iArraylist[i]);
+            }
+        }
+    }
+}
