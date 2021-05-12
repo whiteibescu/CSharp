3f23ac19246b7b77b316ce00032e1c0d657a6dca
SungSoo Park <whiteibescu@gmail.com>
2021-05-12 19:45:55 +0900

    latebinding

---
+++ HustarDay07/LateBinding.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Test9
+{
+    delegate void DispChar(int i);
+    class Program
+    {
+        static void DispStar(int i)
+        {
+            Console.Write("*");
+        }
+        static void DispArrow(int i)
+        {
+            Console.Write("->");
+        }
+        static void DispPercent(int i)
+        {
+            Console.WriteLine("{0}%작업완료", i * 10);
+        }
+        static void MyFunc(DispChar dispChar)
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                dispChar(i);
+                Thread.Sleep(100);
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyFunc(DispPercent);
+        }
+    }
+}
