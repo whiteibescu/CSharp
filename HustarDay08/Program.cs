62c91b2bcaf382fddd52f4e3b7d440992af636b8
SungSoo Park <whiteibescu@gmail.com>
2021-05-13 10:13:45 +0900

    try catch 나온 이유

---
+++ HustarDay08/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace HustarDay08
+{
+    class Program
+    {
+        static bool MyFunc1()
+        {
+            Console.WriteLine("Error 발생");
+            return false;
+        }
+
+
+        static bool MyFunc()
+        {
+            Console.WriteLine("MyFunc1() 함수 호출 이전");
+            bool bResult = MyFunc1();
+            if (bResult == false)
+            {
+                return false;
+            }
+            Console.WriteLine("MyFunc1() 함수 호출 이후");
+            return true;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MyFunc() 함수 호출 이전");
+            bool bResult = MyFunc();
+            if (bResult == false)
+            {
+                Console.WriteLine("Error 발생");
+                return;
+            }
+            Console.WriteLine("MyFunc() 함수 호출 이후");
+        }
+    }
+}
