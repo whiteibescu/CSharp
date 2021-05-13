7eeb7e343d2244a1b3ce053e2f02096a338c0078
SungSoo Park <whiteibescu@gmail.com>
2021-05-13 11:59:29 +0900

    Lamda

---
+++ HustarDay08/Delegate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HustarDay08
+{
+
+    class Enemy
+    {
+        public Enemy(string EnemyName)
+        {
+            m_EnemyName = EnemyName;
+            EnemyMgr.StopGameEvent += () => { };
+        }
+
+        private void EnemyMgr_StopGameEvent()
+        {
+            throw new NotImplementedException();
+        }
+        private string m_EnemyName;
+    }
+    delegate void StopGameDelegate();
+
+    class EnemyMgr
+    {
+        public void DiePlayer()
+        {
+            StopGameEvent();
+        }
+        public static event StopGameDelegate StopGameEvent;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EnemyMgr mgr = new EnemyMgr();
+            Enemy m1 = new Enemy("적군1");
+            Enemy m2 = new Enemy("적군2");
+            Enemy m3 = new Enemy("적군3");
+            Enemy m4 = new Enemy("적군4");
+            Enemy m5 = new Enemy("적군5");
+            Enemy m6 = new Enemy("적군6");
+            mgr.DiePlayer();
+        }
+
+    }
+
+}
---
+++ HustarDay08/Lamda.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//배열 intArray를 출력하는 5가지의 방법
+namespace HustarDay08
+{
+    class Lamda
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                Console.WriteLine("intArray[{0}] = {1}", i, intArray[i]);
+            }
+
+            foreach(var i in intArray)
+            {
+                Console.WriteLine("{0}", i);
+            }
+            //Array.ForEach(intArray, MyFunc);
+            /*Array.ForEach(intArray, delegate (int i)
+            {
+                Console.WriteLine("i = {0}", i);
+            });*/
+            //Array.ForEach(intArray, (i) => Console.WriteLine("i = {0}", i));
+            //int Total = intArray.Aggregate(delegate (int i, int j){ return i + j; });
+            int Total = intArray.Aggregate((i, j) => i + j);
+            Console.WriteLine("Total : {0}", Total);
+
+        }
+
+        static int MyFunc(int i, int j)
+        {
+            return i + j;
+        }
+    }
+}
