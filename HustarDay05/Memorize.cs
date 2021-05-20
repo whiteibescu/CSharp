72cb51efa01a96edaf2b42f763b977b45c0045d3
SungSoo Park <whiteibescu@gmail.com>
2021-05-20 10:24:35 +0900

    Repeating

--- HustarDay05/Memorize.cs
+++ HustarDay05/Memorize.cs
@@ -6,57 +6,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalAddressList
-{
-    class Member
-    {
-        public void InputData()
-        {
-            Console.Write("");
-        }
-
-            {
-                return false;
-            }
-            ModifyData();
-            return true;
-        }
-        public void ModifyData()
-        {
-            Console.Write("주소 : ");
-            m_MemAddr = Console.ReadLine();
-            Console.Write("전화번호 : ");
-            m_MemTelNo = Console.ReadLine();
-        }
-            {
-                return false;
-            }
-            ModifyData();
-            return true;
-        }
-        public void ModifyData()
-        {
-            Console.Write("주소 : ");
-            m_MemAddr = Console.ReadLine();
-            Console.Write("전화번호 : ");
-            m_MemTelNo = Console.ReadLine();
-        }
-        public void PrintData()
-        {
-            Console.WriteLine("1. 이름", m_MemName);
-        }
-
-
-
-        private string m_MemName;
-        private string m_MemAdd;
-        private string m_TelNo;
-    }
-
+{ 
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
 
         }
     }
+
+
+
+
+
+
 }
