204c2d5478830230d8277f26964b33850363cb25
SungSoo Park <whiteibescu@gmail.com>
2021-06-02 15:58:56 +0900

    Practice

--- HustarDay07/Practice.cs
+++ HustarDay07/Practice.cs
@@ -4,8 +4,33 @@ using System.Text;
 
 namespace HustarDay07
 {
-    class Practice
+    abstract class Animal
     {
         public abstract Animal Clone();
+        public abstract void Speak();
+        public abstract string GetAnimalType();
+
+        public string GetName()
+        {
+            return m_Name;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("이름 : {0}", m_Name);
+            Console.WriteLine("나이 : {0}", m_iAge);
+        }
+
+        public void InputData()
+        {
+            Console.WR
+        }
+
+
+        private string m_Name;
+        private int m_iAge;
+    }
+    class Practice
+    {
     }
 }
