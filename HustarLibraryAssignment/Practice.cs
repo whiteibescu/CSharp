84f07abd23c5193c3bdb87e8aee84b15bfbca2e3
SungSoo Park <whiteibescu@gmail.com>
2021-06-13 17:14:28 +0900

    Practice

--- HustarLibraryAssignment/Practice.cs
+++ HustarLibraryAssignment/Practice.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace HustarLibraryAssignment
 {
+    delegate bool StringCompareDelegate(string str1, string str2);
+    delegate bool CompareBookDelegate(Book book, string str);
     class Practice
     {
     }
