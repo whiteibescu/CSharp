dd1e84c036ea626ef8b9d1f8e6ef180813e64703
성수 박 <whiteibescu@gmail.com>
2021-08-29 17:11:23 +0900

    reviewing

--- HustarDay05/Memorize.cs
+++ HustarDay05/Memorize.cs
@@ -9,7 +9,41 @@ namespace FinalAddressList
 {
     class Member
     {
-
+        public bool InputData(MemberMgr mgr)
+        {
+            Console.Write("이름 : ");
+            m_MemName = Console.ReadLine();
+            if (mgr.FindMember(m_MemName) != null)
+            {
+                return false;
+            }
+            ModifyData();
+            return true;
+        }
+        public void ModifyData()
+        {
+            Console.Write("주소 : ");
+            m_MemAddr = Console.ReadLine();
+            Console.Write("전화번호 : ");
+            m_MemTelNo = Console.ReadLine();
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("이름 : {0}", m_MemName);
+            Console.WriteLine("주소 : {0}", m_MemAddr);
+            Console.WriteLine("전화번호 : {0}", m_MemTelNo);
+        }
+        public bool CompareName(string SearchName)
+        {
+            return m_MemName == SearchName;
+        }
+        public bool ContainName(string SearchName)
+        {
+            return m_MemName.Contains(SearchName);
+        }
+        private string m_MemName;
+        private string m_MemAddr;
+        private string m_MemTelNo;
     }
 
     class MemberMgr
