7f91cc60c5eec4e4c8b4c97869f7bea97cdd4baa
Sean Park <whiteibescu@gmail.com>
2021-09-11 23:45:09 +0900

    reviewing

--- HustarDay05/Memorize.cs
+++ HustarDay05/Memorize.cs
@@ -12,7 +12,7 @@ namespace FinalAddressList
         public bool InputData(MemberMgr mgr)
         {
             Console.Write("이름 :");
-            if (mgr.FindMember(m_MemName != null))
+            if (mgr.FindMember(m_MemName) != null)
             {
                 return false;
             }
@@ -51,7 +51,133 @@ namespace FinalAddressList
     }
     class MemberMgr
     {
-        MemberMgr
+        public void InputMember()
+        {
+            Member m = new Member();
+            if (m.InputData(this) == true)
+            {
+                MemList.Add(m);
+            }
+            else
+            {
+                Console.WriteLine("이미 등록된 회원입니다.");
+            }
+        }
+        public void PrintMember()
+        {
+            for (int i =0; i < MemList.Count; ++i)
+            {
+                Member m = (Member)MemList[i];
+                m.PrintData();
+            }
+        }
+        public int FindMemberIndex(string SearchName)
+        {
+            for (int i = 0; i < MemList.Count; i++)
+            {
+                Member m = (Member)MemList[i];
+                if (m.CompareName(SearchName))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public Member FindMember(string SearchName)
+        {
+            int iIndex = FindMemberIndex(SearchName);
+            if (iIndex != -1)
+            {
+                return (Member)MemList[iIndex];
+            }
+                return null;
+        }
+
+        public void SearchMember()
+        {
+            Console.Write("찾으려는 이름 : ");
+            string SearchName = Console.ReadLine();
+            Member m = FindMember(SearchName);
+            if (m != null)
+            {
+                m.PrintData();
+            }
+            else
+            {
+                Console.Write("찾으려는 데이터는 없습니다");
+            }
+        }
+
+        public void ModifyMember()
+        {
+            Console.WriteLine("수정하려는 이름 : ");
+            string SearchName = Console.ReadLine();
+            Member m = FindMember(SearchName);
+            if (m != null)
+            {
+                m.ModifyData();
+            }
+            else
+            {
+                Console.WriteLine("수정하려는 데이터는 없습니다.");
+            }
+        }
+
+        public void DeleteMember()
+        {
+            Console.Write("삭제하려는 이름 : ");
+            string SearchName = Console.ReadLine();
+            int iIndex = FindMemberIndex(SearchName);
+            if (iIndex != -1)
+            {
+                MemList.RemoveAt(iIndex);
+            }
+            else
+            {
+                Console.WriteLine("삭제하려는 데이터가 없습니다.");
+            }
+        }
+
+        public ArrayList FindMultiMemberIndex(string SearchName)
+        {
+            ArrayList Result = new ArrayList();
+            for (int i = 0; i < MemList.Count; i++)
+            {
+                Member m = (Member)MemList[i];
+                if (m.ContainName(SearchName))
+                {
+                    Result.Add(i);
+                }
+            }
+            return Result;            
+        }
+
+        public void PrintMember(ArrayList iIndexList)
+        {
+            for (int i = 0; i < iIndexList.Count; i++)
+            {
+                Member m = (Member)MemList[(int)iIndexList[i]];
+                m.PrintData();
+            }
+        }
+
+        public void MultiSearchMember()
+        {
+            Console.Write("찾으려는 이름 문자열: ");
+            string SearchName = Console.ReadLine();
+            ArrayList iIndexList = FindMultiMemberIndex(SearchName);
+        }
+
+        public void DeleteMember(ArrayList iIndexList)
+        {
+            for (int i = 0; i < iIndexList.Count - 1; i++)
+            {
+
+            }
+        }
+
+        private ArrayList MemList = new ArrayList();
     }
     class Program
     {
