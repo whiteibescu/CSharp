4d670686b8389109f6936cf20425af247a6a89de
성수 박 <whiteibescu@gmail.com>
2021-09-13 11:06:24 +0900

    ReadJson

---
+++ ReadJson/ReadJson/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            JObject sonSpec = new JObject(
+                new JProperty("score", 9),
+                new JProperty("name", "손흥민"),
+                new JProperty("number", 7)
+            );
+
+            File.WriteAllText(@"C:\path2.json", sonSpec.ToString());
+
+        }
+
+    }
+}
