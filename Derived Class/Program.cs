32446d60dff5c6439e057ead4d0e8aecf2363f9d
성수 박 <whiteibescu@gmail.com>
2021-09-16 16:23:14 +0900

    Delegate

---
+++ Derived Class/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Derived_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Test();
+        }
+
+        // 델리게이트 정의
+        delegate int MyDelegate(string s);
+
+        void Test()
+        {
+            //델리게이트 객체 생성
+            MyDelegate m = new MyDelegate(StringToInt);
+
+            //델리게이트 객체를 메서드로 전달
+            Run(m);
+        }
+
+        // 델리게이트 대상이 되는 어떤 메서드
+        int StringToInt(string s)
+        {
+            return int.Parse(s);
+        }
+
+        // 델리게이트를 전달 받는 메서드
+        void Run(MyDelegate m)
+        {
+            // 델리게이트로부터 메서드 실행
+            int i = m("123");
+
+            Console.WriteLine(i);
+        }
+    }
+}
