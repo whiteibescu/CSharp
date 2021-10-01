acdfc59ad2812c3dd6800f2521c26e87d53564e6
성수 박 <whiteibescu@gmail.com>
2021-10-01 11:21:08 +0900

    lambda3

---
+++ hustarday05/Memorize.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2
+{
+    delegate int dele(int a);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            dele d = a => a + 1;
+            int k = d(3);
+            Console.WriteLine("k = " + k);
+        }
+    }
+}
