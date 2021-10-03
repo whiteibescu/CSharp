74a602e08e28c5c7da6db3f6aa297a31eaeedfea
Sean Park <whiteibescu@gmail.com>
2021-10-03 23:45:32 +0900

    try catch

--- HustarDay05/Memorize.cs
+++ HustarDay05/Memorize.cs
@@ -6,45 +6,50 @@ using System.Threading.Tasks;
 
 namespace HustarDay08
 {
-    class Member
-    {
-        public string NAME;
-        public string ADDR;
-        public string TELNO;
-        public int MEMGUBUN;
-    }
-    class Memgubun
-    {
-        public int MEMGUBUN;
-        public string MEMGUBUNNAME;
-    }
     class Program
     {
+        static void MyFunc1()
+        {
+            Console.WriteLine("Error 발생");
+            //throw new Exception("MyFunc1에서 Error가 발생했습니다.");
+        }
+        static void MyFunc()
+        {
+            Console.WriteLine("MyFunc1()함수 호출 이전");
+            try
+            {
+                MyFunc1();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("MyFunc() ------------->>> Error 발생");
+                Console.WriteLine("MyFunc() ------------->>> {0}", e.Message);
+                throw new Exception("MyFunc()에서 처리하려 하였으나 처리 못 했습니다. MyFunc1에서 Error가 발생했습니다.");
+            }
+            finally
+            {
+                Console.WriteLine("MyFunc함수 finally 호출");
+            }
+            Console.WriteLine("MyFunc1()함수 호출 이후");
+        }
         static void Main(string[] args)
         {
-            List<Member> MemList = new List<Member>()
+            Console.WriteLine("MyFunc()함수 호출 이전");
+            try
             {
-                new Member() {NAME = "홍길동", ADDR = "서울", TELNO = "123-1234" ,MEMGUBUN = 1},
-                new Member() {NAME = "김길동", ADDR = "부산", TELNO = "234-1234" ,MEMGUBUN = 2},
-                new Member() {NAME = "이길동", ADDR = "대구", TELNO = "345-1234" ,MEMGUBUN = 3},
-                new Member() {NAME = "박길동", ADDR = "포항", TELNO = "456-1234" ,MEMGUBUN = 1},
-                new Member() {NAME = "최길동", ADDR = "광주", TELNO = "567-1234" ,MEMGUBUN = 2}
-            };
-            List<Memgubun> MemGubunList = new List<Memgubun>()
-            {
-                new Memgubun() {MEMGUBUN = 1, MEMGUBUNNAME="정회원"},
-                new Memgubun() {MEMGUBUN = 2, MEMGUBUNNAME="일반회원"},
-                new Memgubun() {MEMGUBUN = 3, MEMGUBUNNAME="임신회원"},
-            };
-
-            var result = from m in MemList
-                         select new { NAME = m.NAME, ADDR = m.ADDR, TELNO = m.TELNO };
-            foreach (var r in result)
-            {
-                Console.WriteLine("NAME : {0}", r.NAME);
-                Console.WriteLine("ADDR : {0}", r.ADDR);
-                Console.WriteLine("TELNO : {0}", r.TELNO);
+                MyFunc();
             }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error 발생");
+                Console.WriteLine(e.Message);
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("Main함수 finally 호출");
+            }
+            Console.WriteLine("MyFunc()함수 호출 이후");
         }
     }
 }
