ba60f835ba71ce56cf7170b3bb73e2e212023e5f
Sean Park <whiteibescu@gmail.com>
2021-11-16 16:57:11 +0900

    Writing Codes

--- HustarDay05/Practice.cs
+++ HustarDay05/Practice.cs
@@ -7,55 +7,32 @@ using System.Threading.Tasks;
 
 namespace Test2
 {
-    class Wing
+    class Member
     {
-        public void Fly()
+        public bool InputData(MemberMgr mgr)
         {
-            Console.WriteLine("날기...");
-        }
-    }
-    class Bird
-    {
-        Wing w = new Wing();
-        public void Fly()
-        {
-            w.Fly();
+            Console.Write("이름 : ");
+            m_MemName = Console.ReadLine();
+            if (mgr.FindMember(m_MemName) != null)
+            {
+                return false;
+            }
+            ModifyData();
+            return true();
         }
 
-        public virtual void Speak()
+        public void ModifyData()
         {
-            Console.WriteLine("짹짹...");
+            Console.Write("주소 : ");
+            
         }
-        public virtual void Alggagi()
-        {
-            Console.WriteLine("알까기...");
-        }
-        private int[] m_iArray = new int[1000];
+        private string m_MemName;
+        private string m_MemAddr;
+        private string m_MemTelNo;
     }
-    class Pegasus
+
+    class MemberMgr
     {
-        public void Gallop()
-        {
-            Console.WriteLine("달리기...");
-        }
-        public void Speak()
-        {
-            Console.WriteLine("히잉~~~");
-        }
-        public void Fly()
-        {
-            m_Wing.Fly();
-        }
-        Wing m_Wing = new Wing();
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Pegasus p = new Pegasus();
-            p.Gallop();
-            p.Fly();
-            p.Speak();
-        }
+
     }
 }
