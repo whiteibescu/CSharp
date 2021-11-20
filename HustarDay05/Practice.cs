88fc64d2c3cb99378bc54d0af4f9a750406b0d30
Sean Park <whiteibescu@gmail.com>
2021-11-20 23:05:31 +0900

    virtual

--- HustarDay05/Practice.cs
+++ HustarDay05/Practice.cs
@@ -7,32 +7,67 @@ using System.Threading.Tasks;
 
 namespace Test2
 {
-    class Member
+    class Animal
     {
-        public bool InputData(MemberMgr mgr)
+        public Animal()
         {
-            Console.Write("이름 : ");
-            m_MemName = Console.ReadLine();
-            if (mgr.FindMember(m_MemName) != null)
-            {
-                return false;
-            }
-            ModifyData();
-            return true();
+            Console.WriteLine("public Animal()생성자 호출");
         }
-
-        public void ModifyData()
+        public virtual void Speak()
         {
-            Console.Write("주소 : ");
-            
+            Console.WriteLine("Animal Speak!!!");
         }
-        private string m_MemName;
-        private string m_MemAddr;
-        private string m_MemTelNo;
+        public virtual void Speak2()
+        {
+            Console.WriteLine("Animal2 Speak!!!");
+        }
+        private int m_iAge;
     }
-
-    class MemberMgr
+    class Cat : Animal
     {
-
+        public Cat()
+        {
+            Console.WriteLine("public Cat()생성자 호출");
+        }
+        public override void Speak2()
+        {
+            Console.WriteLine("야옹2!!!");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("야옹!!!");
+        }
+    }
+    class Dog : Animal
+    {
+        public Dog()
+        {
+            Console.WriteLine("public Dog()생성자 호출");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("멍멍!!!");
+        }
+    }
+    class Horse : Animal
+    {
+        public Horse()
+        {
+            Console.WriteLine("public Horse()생성자 호출");
+        }
+        public override void Speak()
+        {
+            Console.WriteLine("히잉!!!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.Speak();
+            Animal a1 = new Cat();
+            a1.Speak2();
+        }
     }
 }
