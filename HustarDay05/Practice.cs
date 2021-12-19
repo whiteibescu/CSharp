c034b6e7a35c93bc57f5ccb9e74a3a45cb365c3b
Sean Park <whiteibescu@gmail.com>
2021-12-19 17:32:44 +0900

    Singleton

--- HustarDay05/Practice.cs
+++ HustarDay05/Practice.cs
@@ -1,29 +1,97 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace AutoImplementedProperty
+namespace HustarDay05
 {
-    class BirthdayInfo
+    class Weapon
     {
-        public string Name { get; set; } = "unknown";
-        public DateTime Birthday { get; set; } = new DateTime(1, 1, 1);
-        public int Age
+        public virtual void Attack()
         {
-            get
-            {
-                return new DateTime(DateTime.Now.Subtract(Birthday).Ticks).Year;
-            }
         }
     }
+    class Gun : Weapon
+    {
+        private Gun()
+        { }
+        public override void Attack()
+        {
+            Console.WriteLine("총 공격");
+        }
+        public static Weapon CreateObject()
+        {
+            if (m_weapon == null)
+            {
+                m_weapon = new Gun();
+            }
+            return m_weapon;
+        }
+        private static Weapon m_weapon = null;
+    }
+    class Missile : Weapon
+    {
+        private Missile()
+        { }
+        public override void Attack()
+        {
+            Console.WriteLine("미사일 공격");
+        }
+        public static Weapon CreateObject()
+        {
+            if (m_weapon == null)
+            {
+                m_weapon = new Missile();
+            }
+            return m_weapon;
+        }
+        private static Weapon m_weapon = null;
+    }
+    class Sword : Weapon
+    {
+        private Sword()
+        { }
+        public override void Attack()
+        {
+            Console.WriteLine("칼 공격");
+        }
+        public static Weapon CreateObject()
+        {
+            if (m_weapon == null)
+            {
+                m_weapon = new Sword();
+            }
+            return m_weapon;
+        }
+        private static Weapon m_weapon = null;
 
-    class MainApp
+    }
+    class Robot
+    {
+        public void ChangeWeapon(Weapon weapon)
+        {
+            m_Weapon = weapon;
+        }
+        public void Attack()
+        {
+            m_Weapon.Attack();
+        }
+        private Weapon m_Weapon = Gun.CreateObject();
+    }
+    class Program
     {
         static void Main(string[] args)
         {
-            BirthdayInfo birth = new BirthdayInfo();
-
-
-            birth.Name = "서현";
-            birth.Birthday = new DateTime(1991, 12, 16);
+            Robot r = new Robot();
+            r.Attack();
+            r.ChangeWeapon(Missile.CreateObject());
+            r.Attack();
+            r.ChangeWeapon(Sword.CreateObject());
+            r.Attack();
+            r.ChangeWeapon(Missile.CreateObject());
+            r.Attack();
         }
     }
 }
