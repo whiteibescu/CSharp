09909c5219897be65b1b3fc734cdc0a706736bcf
Sean Park <whiteibescu@gmail.com>
2022-01-06 17:54:37 +0900

    using delegate callback

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,31 +1,82 @@
 ﻿using System;
-using System.Collections;
 
-namespace Yield
+namespace UsingCallback
 {
-    class MyEnumerator
-    {
-        int[] numbers = { 1, 2, 3, 4 };
-        public IEnumerator GetEnumerator()
-        {
-            yield return numbers[0];
-            yield return numbers[1];
-            yield return numbers[2];
-            yield break;
-            yield return numbers[3];
-
-        }
-    }
-
+    delegate int Compare(int a, int b);
     class MainApp
     {
+        static int AscendCompare(int a, int b)
+        {
+            if (a > b)
+            {
+                return 1;
+            }
+            else if (a == b)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static int DescendCompare(int a, int b)
+        {
+            if (a < b)
+            {
+                return 1;
+            }
+            else if (a == b)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        static void BubbleSort(int[] Dataset, Compare Comparer)
+        {
+            int i = 0;
+            int j = 0;
+            int temp = 0;
+
+            for (i = 0; i < Dataset.Length; i++)
+            {
+
+            }
+            for (j = 0; j < Dataset.Length - (i + 1); ++j)
+            {
+                if (Comparer(Dataset[j], Dataset[j + 1]) > 0);
+                temp = Dataset[j + 1];
+                Dataset[j + 1] = Dataset[j];
+                Dataset[j] = temp;
+            }
+        }
+
         static void Main(string[] args)
         {
-            var obj = new MyEnumerator();
-            foreach (int i in obj)
+            int[] array = { 3, 7, 4, 2, 10 };
+
+            Console.WriteLine("Sorting ascending...");
+            BubbleSort(array, new Compare(AscendCompare));
+
+            for (int i = 0; i < array.Length; i++)
             {
-                Console.WriteLine(i);
+                Console.Write($"{array[i]}");
             }
+
+            int[] array2 = { 7, 2, 8, 10, 11 };
+            Console.WriteLine("\nSorting descending...");
+            BubbleSort(array2, new Compare(DescendCompare));
+
+            for (int i = 0; i < array2.Length; i++)
+            {
+                Console.WriteLine($"{array2[i]}");
+            }
+            Console.WriteLine();
         }
     }
 }
