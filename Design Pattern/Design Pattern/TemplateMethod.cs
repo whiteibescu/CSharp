8d5d651ba5afdf87f25442b4d60ce8b3e846be3b
Sean Park <whiteibescu@gmail.com>
2022-03-02 16:09:29 +0900

    TemplateMethod RealWorld Code

--- Design Pattern/Design Pattern/TemplateMethod.cs
+++ Design Pattern/Design Pattern/TemplateMethod.cs
@@ -1,63 +1,260 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Design_Pattern
+namespace Template.RealWorld
 {
-    class TemplateMethod
+    /// <summary>
+    /// Template Design Pattern
+    /// </summary>
+
+    public class Program
     {
         public static void Main(string[] args)
         {
-            AbstractClass aA = new ConcreteClassA();
-            aA.TemplateMethod();
+            DataAccessor categories = new Categories();
+            categories.Run(5);
 
-            AbstractClass aB = new ConcreteClassB();
-            aB.TemplateMethod();
+            DataAccessor products = new Products();
+            products.Run(3);
+
+            // Wait for user
 
             Console.ReadKey();
-
         }
     }
 
+    /// <summary>
+    /// The 'AbstractClass' abstract class
+    /// </summary>
 
-    public abstract class AbstractClass
+    public abstract class DataAccessor
     {
-        public abstract void PrimitiveOperation1();
-        public abstract void PrimitiveOperation2();
+        public abstract void Connect();
+        public abstract void Select();
+        public abstract void Process(int top);
+        public abstract void Disconnect();
 
-        public void TemplateMethod()
+        // The 'Template Method' 
+
+        public void Run(int top)
         {
-            PrimitiveOperation1();
-            PrimitiveOperation2();
-            Console.WriteLine("");
+            Connect();
+            Select();
+            Process(top);
+            Disconnect();
         }
     }
 
-    public class ConcreteClassA : AbstractClass
+    /// <summary>
+    /// A 'ConcreteClass' class
+    /// </summary>
+
+    public class Categories : DataAccessor
     {
-        public override void PrimitiveOperation1()
+        private List<string> categories;
+
+        public override void Connect()
         {
-            Console.WriteLine("ConcreteClassA.PrimitiveOperation1()");
+            categories = new List<string>();
         }
 
-        public override void PrimitiveOperation2()
+        public override void Select()
         {
-            Console.WriteLine("ConcreteClassA.PrimitiveOperation2()");
+            categories.Add("Red");
+            categories.Add("Green");
+            categories.Add("Blue");
+            categories.Add("Yellow");
+            categories.Add("Purple");
+            categories.Add("White");
+            categories.Add("Black");
+        }
+
+        public override void Process(int top)
+        {
+            Console.WriteLine("Categories ---- ");
+
+            for(int i = 0; i < top; i++)
+            {
+                Console.WriteLine(categories[i]);
+            }
+            
+            Console.WriteLine();
+        }
+
+        public override void Disconnect()
+        {
+            categories.Clear();
         }
     }
 
-    public class ConcreteClassB : AbstractClass
+    /// <summary>
+    /// A 'ConcreteClass' class
+    /// </summary>
+
+    public class Products : DataAccessor
     {
-        public override void PrimitiveOperation1()
+        private List<string> products;
+
+        public override void Connect()
         {
-            Console.WriteLine("ConcreteClassB.PrimitiveOperation1()");
+            products = new List<string>();
         }
 
-        public override void PrimitiveOperation2()
+        public override void Select()
         {
-            Console.WriteLine("ConcreteClassB.PrimitiveOperation2()");
+            products.Add("Car");
+            products.Add("Bike");
+            products.Add("Boat");
+            products.Add("Truck");
+            products.Add("Moped");
+            products.Add("Rollerskate");
+            products.Add("Stroller");
+        }
+
+        public override void Process(int top)
+        {
+            Console.WriteLine("Products ---- ");
+
+            for (int i = 0; i < top; i++)
+            {
+                Console.WriteLine(products[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public override void Disconnect()
+        {
+            products.Clear();
+        }
+    }
+}
+
+namespace Template.RealWorlds
+{
+    /// <summary>
+    /// Template Design Pattern
+    /// </summary>
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DataAccessor categories = new Categories();
+            categories.Run(5);
+
+            DataAccessor products = new Products();
+            products.Run(3);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'AbstractClass' abstract class
+    /// </summary>
+
+    public abstract class DataAccessor
+    {
+        public abstract void Connect();
+        public abstract void Select();
+        public abstract void Process(int top);
+        public abstract void Disconnect();
+
+        // The 'Template Method' 
+
+        public void Run(int top)
+        {
+            Connect();
+            Select();
+            Process(top);
+            Disconnect();
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteClass' class
+    /// </summary>
+
+    public class Categories : DataAccessor
+    {
+        private List<string> categories;
+
+        public override void Connect()
+        {
+            categories = new List<string>();
+        }
+
+        public override void Select()
+        {
+            categories.Add("Red");
+            categories.Add("Green");
+            categories.Add("Blue");
+            categories.Add("Yellow");
+            categories.Add("Purple");
+            categories.Add("White");
+            categories.Add("Black");
+        }
+
+        public override void Process(int top)
+        {
+            Console.WriteLine("Categories ---- ");
+
+            for (int i = 0; i < top; i++)
+            {
+                Console.WriteLine(categories[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public override void Disconnect()
+        {
+            categories.Clear();
+        }
+    }
+
+    /// <summary>
+    /// A 'ConcreteClass' class
+    /// </summary>
+
+    public class Products : DataAccessor
+    {
+        private List<string> products;
+
+        public override void Connect()
+        {
+            products = new List<string>();
+        }
+
+        public override void Select()
+        {
+            products.Add("Car");
+            products.Add("Bike");
+            products.Add("Boat");
+            products.Add("Truck");
+            products.Add("Moped");
+            products.Add("Rollerskate");
+            products.Add("Stroller");
+        }
+
+        public override void Process(int top)
+        {
+            Console.WriteLine("Products ---- ");
+
+            for (int i = 0; i < top; i++)
+            {
+                Console.WriteLine(products[i]);
+            }
+
+            Console.WriteLine();
+        }
+
+        public override void Disconnect()
+        {
+            products.Clear();
         }
     }
 }
