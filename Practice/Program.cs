3c5afb476fc10d801db3fb19f57f914d2ec27c4f
Sean Park <whiteibescu@gmail.com>
2022-03-07 21:51:38 +0900

    Builder

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,27 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace Strategy.Structural
+namespace DoFactory.GangOfFour.Builder.RealWorld
 {
     /// <summary>
-    /// Strategy Design Pattern
+    /// MainApp startup class for Real-World 
+    /// Builder Design Pattern.
     /// </summary>
 
-    public class Program
+    public class MainApp
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+
+        public static void Main()
         {
-            Context context;
+            VehicleBuilder builder;
 
-            // Three contexts following different strategies
+            // Create shop with vehicle builders
 
-            context = new Context(new ConcreteStrategyA());
-            context.ContextInterface();
+            Shop shop = new Shop();
 
-            context = new Context(new ConcreteStrategyB());
-            context.ContextInterface();
+            // Construct and display vehicles
 
-            context = new Context(new ConcreteStrategyC());
-            context.ContextInterface();
+            builder = new ScooterBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new CarBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
+
+            builder = new MotorCycleBuilder();
+            shop.Construct(builder);
+            builder.Vehicle.Show();
 
             // Wait for user
 
@@ -30,71 +43,174 @@ namespace Strategy.Structural
     }
 
     /// <summary>
-    /// The 'Strategy' abstract class
+    /// The 'Director' class
     /// </summary>
 
-    public abstract class Strategy
+    class Shop
     {
-        public abstract void AlgorithmInterface();
-    }
+        // Builder uses a complex series of steps
 
-    /// <summary>
-    /// A 'ConcreteStrategy' class
-    /// </summary>
-
-    public class ConcreteStrategyA : Strategy
-    {
-        public override void AlgorithmInterface()
+        public void Construct(VehicleBuilder vehicleBuilder)
         {
-            Console.WriteLine(
-                "Called ConcreteStrategyA.AlgorithmInterface()");
+            vehicleBuilder.BuildFrame();
+            vehicleBuilder.BuildEngine();
+            vehicleBuilder.BuildWheels();
+            vehicleBuilder.BuildDoors();
         }
     }
 
     /// <summary>
-    /// A 'ConcreteStrategy' class
+    /// The 'Builder' abstract class
     /// </summary>
 
-    public class ConcreteStrategyB : Strategy
+    abstract class VehicleBuilder
     {
-        public override void AlgorithmInterface()
+        protected Vehicle vehicle;
+
+        // Gets vehicle instance
+
+        public Vehicle Vehicle
         {
-            Console.WriteLine(
-                "Called ConcreteStrategyB.AlgorithmInterface()");
+            get { return vehicle; }
+        }
+
+        // Abstract build methods
+
+        public abstract void BuildFrame();
+        public abstract void BuildEngine();
+        public abstract void BuildWheels();
+        public abstract void BuildDoors();
+    }
+
+    /// <summary>
+    /// The 'ConcreteBuilder1' class
+    /// </summary>
+
+    class MotorCycleBuilder : VehicleBuilder
+    {
+        public MotorCycleBuilder()
+        {
+            vehicle = new Vehicle("MotorCycle");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "MotorCycle Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
         }
     }
 
     /// <summary>
-    /// A 'ConcreteStrategy' class
+    /// The 'ConcreteBuilder2' class
     /// </summary>
 
-    public class ConcreteStrategyC : Strategy
+    class CarBuilder : VehicleBuilder
     {
-        public override void AlgorithmInterface()
+        public CarBuilder()
         {
-            Console.WriteLine(
-                "Called ConcreteStrategyC.AlgorithmInterface()");
+            vehicle = new Vehicle("Car");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Car Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "2500 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "4";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "4";
         }
     }
 
     /// <summary>
-    /// The 'Context' class
+    /// The 'ConcreteBuilder3' class
     /// </summary>
 
-    public class Context
+    class ScooterBuilder : VehicleBuilder
     {
-        Strategy strategy;
+        public ScooterBuilder()
+        {
+            vehicle = new Vehicle("Scooter");
+        }
+
+        public override void BuildFrame()
+        {
+            vehicle["frame"] = "Scooter Frame";
+        }
+
+        public override void BuildEngine()
+        {
+            vehicle["engine"] = "50 cc";
+        }
+
+        public override void BuildWheels()
+        {
+            vehicle["wheels"] = "2";
+        }
+
+        public override void BuildDoors()
+        {
+            vehicle["doors"] = "0";
+        }
+    }
+
+    /// <summary>
+    /// The 'Product' class
+    /// </summary>
+
+    class Vehicle
+    {
+        private string _vehicleType;
+        private Dictionary<string, string> _parts =
+          new Dictionary<string, string>();
 
         // Constructor
 
-        public Context(Strategy strategy)
+        public Vehicle(string vehicleType)
         {
-            this.strategy = strategy;
+            this._vehicleType = vehicleType;
         }
 
-        public void ContextInterface()
+        // Indexer
+
+        public string this[string key]
         {
-            strategy.AlgorithmInterface();
+            get { return _parts[key]; }
+            set { _parts[key] = value; }
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("\n---------------------------");
+            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
+            Console.WriteLine(" Frame : {0}", _parts["frame"]);
+            Console.WriteLine(" Engine : {0}", _parts["engine"]);
+            Console.WriteLine(" #Wheels: {0}", _parts["wheels"]);
+            Console.WriteLine(" #Doors : {0}", _parts["doors"]);
         }
     }
 }
