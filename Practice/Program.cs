e7010191f0eadf98d3da0f4491eb9a61447b923c
Sean Park <whiteibescu@gmail.com>
2022-03-16 20:58:20 +0900

    Command Design Pattern

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,65 +1,287 @@
 ﻿using System;
 
-namespace Adapter.Structural
+namespace Command.Structural2
 {
     /// <summary>
-    /// Adapter Design Pattern
+    /// Command Design Pattern
     /// </summary>
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Create adapter and place a request
+            // Create receiver, command, and invoker
 
-            Target target = new Adapter();
-            target.Request();
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
+
+            // Set and execute command
+
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
 
             // Wait for user
 
             Console.ReadKey();
         }
-
     }
 
     /// <summary>
-    /// The 'Target' class
+    /// The 'Command' abstract class
     /// </summary>
 
-    public class Target
+    public abstract class Command
     {
-        public virtual void Request()
+        protected Receiver receiver;
+
+        // Constructor
+
+        public Command(Receiver receiver)
         {
-            Console.WriteLine("Called Target Request()");
+            this.receiver = receiver;
+        }
+
+        public abstract void Execute();
+    }
+
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+
+    public class ConcreteCommand : Command
+    {
+        // Constructor
+
+        public ConcreteCommand(Receiver receiver) :
+            base(receiver)
+        {
+        }
+
+        public override void Execute()
+        {
+            receiver.Action();
         }
     }
 
     /// <summary>
-    /// The 'Adapter' class
+    /// The 'Receiver' class
     /// </summary>
 
-    public class Adapter : Target
+    public class Receiver
     {
-        private Adaptee adaptee = new Adaptee();
-
-        public override void Request()
+        public void Action()
         {
-            // Possibly do some other work
-            // and then call SpecificRequest
-
-            adaptee.SpecificRequest();
+            Console.WriteLine("Called Receiver.Action()");
         }
     }
 
     /// <summary>
-    /// The 'Adaptee' class
+    /// The 'Invoker' class
     /// </summary>
 
-    public class Adaptee
+    public class Invoker
     {
-        public void SpecificRequest()
+        Command command;
+
+        public void SetCommand(Command command)
         {
-            Console.WriteLine("Called SpecificRequest()");
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            command.Execute();
+        }
+    }
+}
+
+namespace Command.RealWorld
+{
+    /// <summary>
+    /// Command Design Pattern
+    /// </summary>
+
+    public class Program23
+    {
+        public static void Main(string[] args)
+        {
+            // Create user and let her compute
+
+            User user = new User();
+
+            // User presses calculator buttons
+
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+
+            // Undo 4 commands
+
+            user.Undo(4);
+
+            // Redo 3 commands
+
+            user.Redo(3);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Command' abstract class
+    /// </summary>
+
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+
+    public class CalculatorCommand : Command
+    {
+        char @operator;
+        int operand;
+        Calculator calculator;
+
+        // Constructor
+
+        public CalculatorCommand(Calculator calculator,
+            char @operator, int operand)
+        {
+            this.calculator = calculator;
+            this.@operator = @operator;
+            this.operand = operand;
+        }
+
+        // Gets operator
+
+        public char Operator
+        {
+            set { @operator = value; }
+        }
+
+        // Get operand
+
+        public int Operand
+        {
+            set { operand = value; }
+        }
+
+        // Execute new command
+
+        public override void Execute()
+        {
+            calculator.Operation(@operator, operand);
+        }
+
+        // Unexecute last command
+
+        public override void UnExecute()
+        {
+            calculator.Operation(Undo(@operator), operand);
+        }
+
+        // Returns opposite operator for given operator
+
+        private char Undo(char @operator)
+        {
+            switch (@operator)
+            {
+                case '+': return '-';
+                case '-': return '+';
+                case '*': return '/';
+                case '/': return '*';
+                default:
+                    throw new
+             ArgumentException("@operator");
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+
+    public class Calculator
+    {
+        int curr = 0;
+
+        public void Operation(char @operator, int operand)
+        {
+            switch (@operator)
+            {
+                case '+': curr += operand; break;
+                case '-': curr -= operand; break;
+                case '*': curr *= operand; break;
+                case '/': curr /= operand; break;
+            }
+            Console.WriteLine(
+                "Current value = {0,3} (following {1} {2})",
+                curr, @operator, operand);
+        }
+    }
+
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+
+    public class User
+    {
+        // Initializers
+
+        Calculator calculator = new Calculator();
+        List<Command> commands = new List<Command>();
+        int current = 0;
+
+        public void Redo(int levels)
+        {
+            Console.WriteLine("\n---- Redo {0} levels ", levels);
+            // Perform redo operations
+            for (int i = 0; i < levels; i++)
+            {
+                if (current < commands.Count - 1)
+                {
+                    Command command = commands[current++];
+                    command.Execute();
+                }
+            }
+        }
+
+        public void Undo(int levels)
+        {
+            Console.WriteLine("\n---- Undo {0} levels ", levels);
+
+            // Perform undo operations
+
+            for (int i = 0; i < levels; i++)
+            {
+                if (current > 0)
+                {
+                    Command command = commands[--current] as Command;
+                    command.UnExecute();
+                }
+            }
+        }
+
+        public void Compute(char @operator, int operand)
+        {
+            // Create command operation and execute it
+
+            Command command = new CalculatorCommand(calculator, @operator, operand);
+            command.Execute();
+
+            // Add command to undo list
+
+            commands.Add(command);
+            current++;
         }
     }
 }
