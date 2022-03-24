82afefca38231aff184bbcd9972b2f0c3efddd76
Sean Park <whiteibescu@gmail.com>
2022-03-24 14:53:42 +0900

    Adapter Structural Pattern

--- Design Pattern/Design Pattern/Adapter.cs
+++ Design Pattern/Design Pattern/Adapter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 
-namespace Adapter.RealWorld
+namespace Adapter.Structural
 {
     /// <summary>
     /// Adapter Design Pattern
@@ -10,42 +10,27 @@ namespace Adapter.RealWorld
     {
         public static void Main(string[] args)
         {
-            // Non-adapted chemical compound
+            // Create adapter and place a request
 
-            Compound unknown = new Compound();
-            unknown.Display();
-
-            // Adapted chemical compounds
-
-            Compound water = new RichCompound("Water");
-            water.Display();
-
-            Compound benzene = new RichCompound("Benzene");
-            benzene.Display();
-
-            Compound ethanol = new RichCompound("Ethanol");
-            ethanol.Display();
+            Target target = new Adapter();
+            target.Request();
 
             // Wait for user
 
             Console.ReadKey();
         }
+
     }
 
     /// <summary>
     /// The 'Target' class
     /// </summary>
 
-    public class Compound
+    public class Target
     {
-        protected float boilingPoint;
-        protected float meltingPoint;
-        protected double molecularWeight;
-        protected string molecularFormula;
-
-        public virtual void Display()
+        public virtual void Request()
         {
-            Console.WriteLine("\nCompound: Unknown ------ ");
+            Console.WriteLine("Called Target Request()");
         }
     }
 
@@ -53,34 +38,16 @@ namespace Adapter.RealWorld
     /// The 'Adapter' class
     /// </summary>
 
-    public class RichCompound : Compound
+    public class Adapter : Target
     {
-        private string chemical;
-        private ChemicalDatabank bank;
+        private Adaptee adaptee = new Adaptee();
 
-        // Constructor
-
-        public RichCompound(string chemical)
+        public override void Request()
         {
-            this.chemical = chemical;
-        }
+            // Possibly do some other work
+            // and then call SpecificRequest
 
-        public override void Display()
-        {
-            // The Adaptee
-
-            bank = new ChemicalDatabank();
-
-            boilingPoint = bank.GetCriticalPoint(chemical, "B");
-            meltingPoint = bank.GetCriticalPoint(chemical, "M");
-            molecularWeight = bank.GetMolecularWeight(chemical);
-            molecularFormula = bank.GetMolecularStructure(chemical);
-
-            Console.WriteLine("\nCompound: {0} ------ ", chemical);
-            Console.WriteLine(" Formula: {0}", molecularFormula);
-            Console.WriteLine(" Weight : {0}", molecularWeight);
-            Console.WriteLine(" Melting Pt: {0}", meltingPoint);
-            Console.WriteLine(" Boiling Pt: {0}", boilingPoint);
+            adaptee.SpecificRequest();
         }
     }
 
@@ -88,58 +55,11 @@ namespace Adapter.RealWorld
     /// The 'Adaptee' class
     /// </summary>
 
-    public class ChemicalDatabank
+    public class Adaptee
     {
-        // The databank 'legacy API'
-
-        public float GetCriticalPoint(string compound, string point)
+        public void SpecificRequest()
         {
-            // Melting Point
-            if (point == "M")
-            {
-                switch (compound.ToLower())
-                {
-                    case "water": return 0.0f;
-                    case "benzene": return 5.5f;
-                    case "ethanol": return -114.1f;
-                    default: return 0f;
-                }
-            }
-
-            // Boiling Point
-
-            else
-            {
-                switch (compound.ToLower())
-                {
-                    case "water": return 100.0f;
-                    case "benzene": return 80.1f;
-                    case "ethanol": return 78.3f;
-                    default: return 0f;
-                }
-            }
-        }
-
-        public string GetMolecularStructure(string compound)
-        {
-            switch (compound.ToLower())
-            {
-                case "water": return "H20";
-                case "benzene": return "C6H6";
-                case "ethanol": return "C2H5OH";
-                default: return "";
-            }
-        }
-
-        public double GetMolecularWeight(string compound)
-        {
-            switch (compound.ToLower())
-            {
-                case "water": return 18.015;
-                case "benzene": return 78.1134;
-                case "ethanol": return 46.0688;
-                default: return 0d;
-            }
+            Console.WriteLine("Called SpecificRequest()");
         }
     }
 }
