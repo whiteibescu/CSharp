4938f7489102b8316d99f1f32e37ad9dd82b0053
Sean Park <whiteibescu@gmail.com>
2022-03-28 21:25:45 +0900

    Momento Pattern

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,249 +1,90 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Interpreter.Structural
+
+namespace Memento.Structural
 {
     /// <summary>
-    /// Interpreter Design Pattern
+    /// Memento Design Pattern
     /// </summary>
-
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Context context = new Context();
+            Originator o = new Originator();
+            o.State = "On";
 
-            // Usually a tree 
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
 
-            List<AbstractExpression> list = new List<AbstractExpression>();
+            o.State = "Off";
 
-            // Populate 'abstract syntax tree' 
-
-            list.Add(new TerminalExpression());
-            list.Add(new NonterminalExpression());
-            list.Add(new TerminalExpression());
-            list.Add(new TerminalExpression());
-
-            // Interpret
-
-            foreach (AbstractExpression exp in list)
-            {
-                exp.Interpret(context);
-            }
-
-            // Wait for user
+            o.SetMemento(c.Memento);
 
             Console.ReadKey();
+
         }
     }
 
     /// <summary>
-    /// The 'Context' class
+    /// The 'Originator' class
     /// </summary>
 
-    public class Context
+    public class Originator
     {
-    }
+        string state;
 
-    /// <summary>
-    /// The 'AbstractExpression' abstract class
-    /// </summary>
-
-    public abstract class AbstractExpression
-    {
-        public abstract void Interpret(Context context);
-    }
-
-    /// <summary>
-    /// The 'TerminalExpression' class
-    /// </summary>
-
-    public class TerminalExpression : AbstractExpression
-    {
-        public override void Interpret(Context context)
+        public string State
         {
-            Console.WriteLine("Called Terminal.Interpret()");
-        }
-    }
-
-    /// <summary>
-    /// The 'NonterminalExpression' class
-    /// </summary>
-
-    public class NonterminalExpression : AbstractExpression
-    {
-        public override void Interpret(Context context)
-        {
-            Console.WriteLine("Called Nonterminal.Interpret()");
-        }
-    }
-
-    /// RealWorld
-    /// Interpreter Design Pattern
-    /// </summary>
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            string roman = "MCMXXVIII";
-            Context context = new Context(roman);
-
-            // Build the 'parse tree'
-
-            List<Expression> tree = new List<Expression>();
-            tree.Add(new ThousandExpression());
-            tree.Add(new HundredExpression());
-            tree.Add(new TenExpression());
-            tree.Add(new OneExpression());
-
-            // Interpret
-
-            foreach (Expression exp in tree)
+            get { return state; }
+            set
             {
-                exp.Interpret(context);
-            }
-
-            Console.WriteLine("{0} = {1}",
-                roman, context.Output);
-
-            // Wait for user
-
-            Console.ReadKey();
-        }
-    }
-
-    /// <summary>
-    /// The 'Context' class
-    /// </summary>
-
-    public class Context
-    {
-        string input;
-        int output;
-
-        // Constructor
-
-        public Context(string input)
-        {
-            this.input = input;
-        }
-
-        public string Input
-        {
-            get { return input; }
-            set { input = value; }
-        }
-
-        public int Output
-        {
-            get { return output; }
-            set { output = value; }
-        }
-    }
-
-    /// <summary>
-    /// The 'AbstractExpression' class
-    /// </summary>
-
-    public abstract class Expression
-    {
-        public void Interpret(Context context)
-        {
-            if (context.Input.Length == 0)
-                return;
-
-            if (context.Input.StartsWith(Nine()))
-            {
-                context.Output += (9 * Multiplier());
-                context.Input = context.Input.Substring(2);
-            }
-            else if (context.Input.StartsWith(Four()))
-            {
-                context.Output += (4 * Multiplier());
-                context.Input = context.Input.Substring(2);
-            }
-            else if (context.Input.StartsWith(Five()))
-            {
-                context.Output += (5 * Multiplier());
-                context.Input = context.Input.Substring(1);
-            }
-
-            while (context.Input.StartsWith(One()))
-            {
-                context.Output += (1 * Multiplier());
-                context.Input = context.Input.Substring(1);
+                state = value;
+                Console.WriteLine("Satate = " + state);
             }
         }
 
-        public abstract string One();
-        public abstract string Four();
-        public abstract string Five();
-        public abstract string Nine();
-        public abstract int Multiplier();
+        public Memento CreateMemento()
+        {
+            return (new Memento(state));
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+    /// <summary>
+    /// The 'Memento' class
+    /// </summary>
+    public class Memento
+    {
+        string state;
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        public string State
+        {
+            get { return state; }
+        }
     }
 
     /// <summary>
-    /// A 'TerminalExpression' class
-    /// <remarks>
-    /// Thousand checks for the Roman Numeral M 
-    /// </remarks>
+    /// The 'Caretaker' class
     /// </summary>
 
-    public class ThousandExpression : Expression
+
+    public class Caretaker
     {
-        public override string One() { return "M"; }
-        public override string Four() { return " "; }
-        public override string Five() { return " "; }
-        public override string Nine() { return " "; }
-        public override int Multiplier() { return 1000; }
-    }
+        Memento memento;
 
-    /// <summary>
-    /// A 'TerminalExpression' class
-    /// <remarks>
-    /// Hundred checks C, CD, D or CM
-    /// </remarks>
-    /// </summary>
-
-    public class HundredExpression : Expression
-    {
-        public override string One() { return "C"; }
-        public override string Four() { return "CD"; }
-        public override string Five() { return "D"; }
-        public override string Nine() { return "CM"; }
-        public override int Multiplier() { return 100; }
-    }
-
-    /// <summary>
-    /// A 'TerminalExpression' class
-    /// <remarks>
-    /// Ten checks for X, XL, L and XC
-    /// </remarks>
-    /// </summary>
-
-    public class TenExpression : Expression
-    {
-        public override string One() { return "X"; }
-        public override string Four() { return "XL"; }
-        public override string Five() { return "L"; }
-        public override string Nine() { return "XC"; }
-        public override int Multiplier() { return 10; }
-    }
-
-    /// <summary>
-    /// A 'TerminalExpression' class
-    /// <remarks>
-    /// One checks for I, II, III, IV, V, VI, VI, VII, VIII, IX
-    /// </remarks>
-    /// </summary>
-
-    public class OneExpression : Expression
-    {
-        public override string One() { return "I"; }
-        public override string Four() { return "IV"; }
-        public override string Five() { return "V"; }
-        public override string Nine() { return "IX"; }
-        public override int Multiplier() { return 1; }
+        public Memento Memento
+        {
+            set { memento = value; }
+            get { return memento; }
+        }
     }
 }
