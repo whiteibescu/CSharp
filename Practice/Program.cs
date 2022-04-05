240f08ca64806cd5212628015f887e4f1f9f0502
Sean Park <whiteibescu@gmail.com>
2022-04-05 21:34:21 +0900

    Making a book manager program

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,121 +1,270 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
+using System.Text;
 
-namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+namespace BookManager
 {
-    public interface IObserver
+    delegate bool StringCompareDelegate(string str1, string str2);
+    delegate bool CompareBookDelegate(Book book, string str);
+
+    static class BookUtil
     {
-        // Receive update from subject
-        void Update(ISubject subject);
-    }
-
-    public interface ISubject
-    {
-        // Attach an observer to the subject.
-        void Attach(IObserver observer);
-
-        // Detach an observer from the subject.
-        void Detach(IObserver observer);
-
-        // Notify all observers about an event.
-        void Notify();
-    }
-
-    // The Subject owns some important state and notifies observers when the
-    // state changes.
-    public class Subject : ISubject
-    {
-        // For the sake of simplicity, the Subject's state, essential to all
-        // subscribers, is stored in this variable.
-        public int State { get; set; } = -0;
-
-        // List of subscribers. In real life, the list of subscribers can be
-        // stored more comprehensively (categorized by event type, etc.).
-        private List<IObserver> _observers = new List<IObserver>();
-
-        // The subscription management methods.
-        public void Attach(IObserver observer)
+        public static int GetInputInt()
         {
-            Console.WriteLine("Subject: Attached an observer.");
-            this._observers.Add(observer);
-        }
-
-        public void Detach(IObserver observer)
-        {
-            this._observers.Remove(observer);
-            Console.WriteLine("Subject: Detached an observer.");
-        }
-
-        // Trigger an update in each subscriber.
-        public void Notify()
-        {
-            Console.WriteLine("Subject: Notifying observers...");
-
-            foreach (var observer in _observers)
+            int iChoice = 0;
+            while (true)
             {
-                observer.Update(this);
+                try
+                {
+                    iChoice = int.Parse(Console.ReadLine());
+                    return iChoice;
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("숫자값을 입력하세요...");
+                }
             }
         }
 
-        // Usually, the subscription logic is only a fraction of what a Subject
-        // can really do. Subjects commonly hold some important business logic,
-        // that triggers a notification method whenever something important is
-        // about to happen (or after it).
-        public void SomeBusinessLogic()
+        public static string InputString(string strTitle)
         {
-            Console.WriteLine("\nSubject: I'm doing something important.");
-            this.State = new Random().Next(0, 10);
-
-            Thread.Sleep(15);
-
-            Console.WriteLine("Subject: My state has just changed to: " + this.State);
-            this.Notify();
+            Console.Write(strTitle);
+            return Console.ReadLine();
         }
-    }
 
-    // Concrete Observers react to the updates issued by the Subject they had
-    // been attached to.
-    class ConcreteObserverA : IObserver
-    {
-        public void Update(ISubject subject)
+        public static List<string> InputList(string strTitle)
         {
-            if ((subject as Subject).State < 3)
+            List<string> strList = new List<string>();
+            int iIndex = 1;
+            while (true)
             {
-                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+                Console.Write($"{strTitle}{iIndex} : ");
+                string strString = Console.ReadLine();
+                if (strString == "")
+                {
+                    return strList;
+                }
+                ++iIndex;
+                strList.Add(strString);
             }
         }
-    }
-
-    class ConcreteObserverB : IObserver
-    {
-        public void Update(ISubject subject)
+        public static void PrintList(string strTitle, List<string> strList)
         {
-            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            for (int i = 0; i < strList.Count; i++)
             {
-                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+                Console.WriteLine($"{strTitle}{i + 1} : {strList[i]}");
             }
         }
+
+        public static bool StringEqual(string str1, string str2)
+        {
+            return str1 == str2;
+        }
+
+        public static bool StringContains(string str1, string str2)
+        {
+            return str1.Contains(str2);
+        }
+
+        public static bool SearchInList(List<string> strList, string str, StringCompareDelegate compareDelegate)
+        {
+            for (int i = 0; i < strList.Count; i++)
+            {
+                if (compareDelegate(strList[i], str))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 
-    class Program
+    interface IBookMgr
     {
-        static void Main(string[] args)
+        int SearchBook(string strISBN, CompareBookDelegate compareBookDelegate);
+    }
+
+    class Book
+    {
+        public void ModifyData()
         {
-            // The client code.
-            var subject = new Subject();
-            var observerA = new ConcreteObserverA();
-            subject.Attach(observerA);
+            m_BookTitle = BookUtil.InputString("책 제목");
+            m_ISBN = BookUtil.InputString("저자");
 
-            var observerB = new ConcreteObserverB();
-            subject.Attach(observerB);
-
-            subject.SomeBusinessLogic();
-            subject.SomeBusinessLogic();
-
-            subject.Detach(observerB);
-
-            subject.SomeBusinessLogic();
         }
+        public bool InputData(IBookMgr bookMgr)
+        {
+            m_ISBN = BookUtil.InputString("ISBN : ");
+            if (bookMgr.SearchBook(m_ISBN, Book.CompareISBN) != -1)
+                ModifyData();
+            return true;
+        }
+        public void PrintData()
+        {
+            Console.WriteLine("ISBN : {0}", m_ISBN);
+            Console.WriteLine("책제목 : {0}", m_BookTitle);
+            BookUtil.PrintList("저자", m_BookAuthors);
+            BookUtil.PrintList("목차", m_BookContents);
+            Console.WriteLine("출판사 : {0}", m_BookPublisher);
+        }
+        public static bool CompareISBN(Book book, string strISBN)
+        {
+            return book.m_ISBN == strISBN;
+        }
+        public static bool CompareBookTitle(Book book, string strTitle)
+        {
+            return book.m_BookTitle == strTitle;
+        }
+        public static bool FindAuthor(Book book, string strAuthor)
+        {
+            return BookUtil.SearchInList(book.m_BookAuthors, strAuthor, BookUtil.StringEqual);
+        }
+        public static bool FindContents(Book book, string strContents)
+        {
+            return BookUtil.SearchInList(book.m_BookContents, strContents, BookUtil.StringContains);
+        }
+        public string GetTitleAndAuthors()
+        {
+            StringBuilder strBuilder = new StringBuilder();
+            strBuilder.Append("책제목 : ");
+            strBuilder.Append(m_BookTitle);
+            strBuilder.Append("저자 : ");
+            for (int i = 0; i < m_BookAuthors.Count; i++)
+            {
+                strBuilder.Append(m_BookAuthors[i]);
+                strBuilder.Append(",");
+            }
+            return strBuilder.ToString();
+        }
+
+        
+
+        private string m_BookTitle;
+        private string m_ISBN;
+        private List<string> m_BookAuthors;
+        private List<string> m_BookContents;
+        private string m_BookPublisher;
+    }
+    class BookManager : IBookMgr
+    {
+        public void InsertBook()
+        {
+            Book b = new Book();
+            if (b.InputData(this))
+            {
+                m_BookList.Add(b);
+            }
+        }
+        public void PrintAllBooks()
+        {
+            for (int i = 0; i < m_BookList.Count; i++)
+            {
+                m_BookList[i].PrintData();
+            }
+        }
+        public void PrintAllBooks(List<int> ResultList)
+        {
+            for (int i = 0; i < ResultList.Count; i++)
+            {
+                m_BookList[ResultList[i]].PrintData();
+            }
+        }
+        public int SearchBook(string strISBN, CompareBookDelegate compareBookDelegate)
+        {
+            for (int i = 0; i < m_BookList.Count; i++)
+            {
+                if (compareBookDelegate(m_BookList[i], strISBN))
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        public void SearchBook(string strTitle, string strErrMsg, CompareBookDelegate compareBookDelegate)
+        {
+            string str = BookUtil.InputString(strTitle);
+            int iIndex = SearchBook(str, compareBookDelegate);
+            if (iIndex != -1)
+            {
+                m_BookList[iIndex].PrintData();
+            }
+            else
+            {
+                Console.WriteLine(strErrMsg);
+            }
+        }
+        public List<int> SearchInList(string str, CompareBookDelegate compareBookDelegate)
+        {
+            List<int> ResultList = new List<int>();
+            for (int i = 0; i < m_BookList.Count; i++)
+            {
+                if (compareBookDelegate(m_BookList[i], str))
+                {
+                    ResultList.Add(i);
+                }
+            }
+            return ResultList;
+        }
+
+        public void SearchInList(string strTitle, string strErrMsg, CompareBookDelegate compareBookDelegate)
+        {
+            string strAuthor = BookUtil.InputString(strTitle);
+            List<int> ResultList = SearchInList(strAuthor, compareBookDelegate);
+            if (ResultList.Count > 0)
+            {
+                PrintAllBooks(ResultList);
+            }
+            else
+            {
+                Console.WriteLine(strErrMsg);
+            }
+        }
+
+        public void SearchBookMenu()
+        {
+            int iChoice = 1;
+            while (iChoice != 0)
+            {
+                Console.WriteLine("1. ISBN으로 검색");
+                Console.WriteLine("2. 책제목으로 검색");
+                Console.WriteLine("3. 저자로 검색");
+                Console.WriteLine("4. 목차로 검색");
+                Console.WriteLine("5. 종료");
+                iChoice = BookUtil.GetInputInt();
+                switch (iChoice)
+                {
+                    case 1:
+                        SearchBook("찾을 ISBN코드 : ", "찾으려는 ISBN코드 도서는 존재하지 않습니다.", Book.CompareISBN);
+                        break;
+                    case 2:
+                        SearchInList("찾을 책제목 : ", "찾으려는 제목의 도서는 존재하지 않습니다.", Book.CompareBookTitle);
+                        break;
+                    case 3:
+                        SearchInList("찾으려는 저자 : ", "찾으려는 저자의 도서는 존재하지 않습니다.", Book.FindAuthor);
+                        break;
+                    case 4:
+                        SearchInList("찾으려는 목차 문자열 : ", "찾으려는 목차의 문자열 도서는 존재하지 않습니다.", Book.FindContents);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public int DisplayTitleAndAuthorsList(List<int> Result)
+        {
+            for (int i = 0; i < Result.Count; i++)
+            {
+                Console.WriteLine("{0}. {1}", i + 1, m_BookList[Result[i]].GetTitleAndAuthors());
+            }
+            int iChoice = BookUtil.GetInputInt();
+            if (iChoice > 0 && iChoice <= Result.Count)
+            {
+                return iChoice - 1;
+            }
+            return -1;
+        }
+
+        private List<Book> m_BookList = new List<Book>();
     }
 }
