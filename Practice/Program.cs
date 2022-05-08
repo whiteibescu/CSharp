80947e2d79b8f8f43a9b1502382886997e6fe3a1
Sean Park <whiteibescu@gmail.com>
2022-05-08 11:42:39 +0900

    Abstract Factory

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,191 +1,164 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace Visitor.RealWorld
+namespace DoFactory.GangOfFour.Abstract.RealWorld
 {
     /// <summary>
-    /// Visitor Design Pattern
+    /// MainApp startup class for Real-World
+    /// Abstract Factory Design Pattern.
     /// </summary>
 
-    public class Program
+    class MainApp
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+
+        public static void Main()
         {
-            // Setup employee collection
+            // Create and run the African animal world
 
-            Employees employee = new Employees();
-            employee.Attach(new Clerk());
-            employee.Attach(new Director());
-            employee.Attach(new President());
+            ContinentFactory africa = new AfricaFactory();
+            AnimalWorld world = new AnimalWorld(africa);
+            world.RunFoodChain();
 
-            // Employees are 'visited'
+            // Create and run the American animal world
 
-            employee.Accept(new IncomeVisitor());
-            employee.Accept(new VacationVisitor());
+            ContinentFactory america = new AmericaFactory();
+            world = new AnimalWorld(america);
+            world.RunFoodChain();
 
-            // Wait for user
+            // Wait for user input
 
             Console.ReadKey();
         }
     }
 
+
     /// <summary>
-    /// The 'Visitor' interface
+    /// The 'AbstractFactory' abstract class
     /// </summary>
 
-    public interface IVisitor
+    abstract class ContinentFactory
     {
-        void Visit(Element element);
+        public abstract Herbivore CreateHerbivore();
+        public abstract Carnivore CreateCarnivore();
     }
 
     /// <summary>
-    /// A 'ConcreteVisitor' class
+    /// The 'ConcreteFactory1' class
     /// </summary>
 
-    public class IncomeVisitor : IVisitor
+    class AfricaFactory : ContinentFactory
     {
-        public void Visit(Element element)
+        public override Herbivore CreateHerbivore()
         {
-            Employee employee = element as Employee;
-
-            // Provide 10% pay raise
-
-            employee.Income *= 1.10;
-
-            Console.WriteLine("{0} {1}'s new income: {2:C}",
-                employee.GetType().Name, employee.Name,
-                employee.Income);
+            return new Wildebeest();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Lion();
         }
     }
 
     /// <summary>
-    /// A 'ConcreteVisitor' class
+    /// The 'ConcreteFactory2' class
     /// </summary>
 
-    public class VacationVisitor : IVisitor
+    class AmericaFactory : ContinentFactory
     {
-        public void Visit(Element element)
+        public override Herbivore CreateHerbivore()
         {
-            Employee employee = element as Employee;
-
-            // Provide 3 extra vacation days
-
-            employee.VacationDays += 3;
-
-            Console.WriteLine("{0} {1}'s new vacation days: {2}",
-                employee.GetType().Name, employee.Name,
-                employee.VacationDays);
+            return new Bison();
+        }
+        public override Carnivore CreateCarnivore()
+        {
+            return new Wolf();
         }
     }
 
     /// <summary>
-    /// The 'Element' abstract class
+    /// The 'AbstractProductA' abstract class
     /// </summary>
 
-    public abstract class Element
+    abstract class Herbivore
     {
-        public abstract void Accept(IVisitor visitor);
     }
 
     /// <summary>
-    /// The 'ConcreteElement' class
+    /// The 'AbstractProductB' abstract class
     /// </summary>
 
-    public class Employee : Element
+    abstract class Carnivore
     {
-        private string name;
-        private double income;
-        private int vacationDays;
+        public abstract void Eat(Herbivore h);
+    }
+
+    /// <summary>
+    /// The 'ProductA1' class
+    /// </summary>
+
+    class Wildebeest : Herbivore
+    {
+    }
+
+    /// <summary>
+    /// The 'ProductB1' class
+    /// </summary>
+
+    class Lion : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Wildebeest
+
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+
+    class Bison : Herbivore
+    {
+    }
+
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+
+    class Wolf : Carnivore
+    {
+        public override void Eat(Herbivore h)
+        {
+            // Eat Bison
+
+            Console.WriteLine(this.GetType().Name +
+              " eats " + h.GetType().Name);
+        }
+    }
+
+    /// <summary>
+    /// The 'Client' class 
+    /// </summary>
+
+    class AnimalWorld
+    {
+        private Herbivore _herbivore;
+        private Carnivore _carnivore;
 
         // Constructor
 
-        public Employee(string name, double income,
-            int vacationDays)
+        public AnimalWorld(ContinentFactory factory)
         {
-            this.name = name;
-            this.income = income;
-            this.vacationDays = vacationDays;
+            _carnivore = factory.CreateCarnivore();
+            _herbivore = factory.CreateHerbivore();
         }
 
-        public string Name
+        public void RunFoodChain()
         {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public double Income
-        {
-            get { return income; }
-            set { income = value; }
-        }
-
-        public int VacationDays
-        {
-            get { return vacationDays; }
-            set { vacationDays = value; }
-        }
-
-        public override void Accept(IVisitor visitor)
-        {
-            visitor.Visit(this);
-        }
-    }
-
-    /// <summary>
-    /// The 'ObjectStructure' class
-    /// </summary>
-
-    public class Employees
-    {
-        private List<Employee> employees = new List<Employee>();
-
-        public void Attach(Employee employee)
-        {
-            employees.Add(employee);
-        }
-
-        public void Detach(Employee employee)
-        {
-            employees.Remove(employee);
-        }
-
-        public void Accept(IVisitor visitor)
-        {
-            foreach (Employee employee in employees)
-            {
-                employee.Accept(visitor);
-            }
-            Console.WriteLine();
-        }
-    }
-
-    // Three employee types
-
-    public class Clerk : Employee
-    {
-        // Constructor
-
-        public Clerk()
-            : base("Kevin", 25000.0, 14)
-        {
-        }
-    }
-
-    public class Director : Employee
-    {
-        // Constructor
-        public Director()
-            : base("Elly", 35000.0, 16)
-        {
-        }
-    }
-
-    public class President : Employee
-    {
-        // Constructor
-        public President()
-            : base("Eric", 45000.0, 21)
-        {
+            _carnivore.Eat(_herbivore);
         }
     }
 }
