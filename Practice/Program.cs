b1a18cae152870cbb5c116da66ed13664fc2630c
Sean Park <whiteibescu@gmail.com>
2022-05-12 16:42:11 +0900

    Chain

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,40 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace DoFactory.GangOfFour.Builder.RealWorld
+namespace Command.Structural
 {
     /// <summary>
-    /// MainApp startup class for Real-World 
-    /// Builder Design Pattern.
+    /// Command Design Pattern
     /// </summary>
 
-    public class MainApp
+    public class Program
     {
-        /// <summary>
-        /// Entry point into console application.
-        /// </summary>
-
-        public static void Main()
+        public static void Main(string[] args)
         {
-            VehicleBuilder builder;
+            // Create receiver, command, and invoker
 
-            // Create shop with vehicle builders
+            Receiver receiver = new Receiver();
+            Command command = new ConcreteCommand(receiver);
+            Invoker invoker = new Invoker();
 
-            Shop shop = new Shop();
+            // Set and execute command
 
-            // Construct and display vehicles
-
-            builder = new ScooterBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
-
-            builder = new CarBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
-
-            builder = new MotorCycleBuilder();
-            shop.Construct(builder);
-            builder.Vehicle.Show();
+            invoker.SetCommand(command);
+            invoker.ExecuteCommand();
 
             // Wait for user
 
@@ -43,174 +29,259 @@ namespace DoFactory.GangOfFour.Builder.RealWorld
     }
 
     /// <summary>
-    /// The 'Director' class
+    /// The 'Command' abstract class
     /// </summary>
 
-    class Shop
+    public abstract class Command
     {
-        // Builder uses a complex series of steps
-
-        public void Construct(VehicleBuilder vehicleBuilder)
-        {
-            vehicleBuilder.BuildFrame();
-            vehicleBuilder.BuildEngine();
-            vehicleBuilder.BuildWheels();
-            vehicleBuilder.BuildDoors();
-        }
-    }
-
-    /// <summary>
-    /// The 'Builder' abstract class
-    /// </summary>
-
-    abstract class VehicleBuilder
-    {
-        protected Vehicle vehicle;
-
-        // Gets vehicle instance
-
-        public Vehicle Vehicle
-        {
-            get { return vehicle; }
-        }
-
-        // Abstract build methods
-
-        public abstract void BuildFrame();
-        public abstract void BuildEngine();
-        public abstract void BuildWheels();
-        public abstract void BuildDoors();
-    }
-
-    /// <summary>
-    /// The 'ConcreteBuilder1' class
-    /// </summary>
-
-    class MotorCycleBuilder : VehicleBuilder
-    {
-        public MotorCycleBuilder()
-        {
-            vehicle = new Vehicle("MotorCycle");
-        }
-
-        public override void BuildFrame()
-        {
-            vehicle["frame"] = "MotorCycle Frame";
-        }
-
-        public override void BuildEngine()
-        {
-            vehicle["engine"] = "500 cc";
-        }
-
-        public override void BuildWheels()
-        {
-            vehicle["wheels"] = "2";
-        }
-
-        public override void BuildDoors()
-        {
-            vehicle["doors"] = "0";
-        }
-    }
-
-    /// <summary>
-    /// The 'ConcreteBuilder2' class
-    /// </summary>
-
-    class CarBuilder : VehicleBuilder
-    {
-        public CarBuilder()
-        {
-            vehicle = new Vehicle("Car");
-        }
-
-        public override void BuildFrame()
-        {
-            vehicle["frame"] = "Car Frame";
-        }
-
-        public override void BuildEngine()
-        {
-            vehicle["engine"] = "2500 cc";
-        }
-
-        public override void BuildWheels()
-        {
-            vehicle["wheels"] = "4";
-        }
-
-        public override void BuildDoors()
-        {
-            vehicle["doors"] = "4";
-        }
-    }
-
-    /// <summary>
-    /// The 'ConcreteBuilder3' class
-    /// </summary>
-
-    class ScooterBuilder : VehicleBuilder
-    {
-        public ScooterBuilder()
-        {
-            vehicle = new Vehicle("Scooter");
-        }
-
-        public override void BuildFrame()
-        {
-            vehicle["frame"] = "Scooter Frame";
-        }
-
-        public override void BuildEngine()
-        {
-            vehicle["engine"] = "50 cc";
-        }
-
-        public override void BuildWheels()
-        {
-            vehicle["wheels"] = "2";
-        }
-
-        public override void BuildDoors()
-        {
-            vehicle["doors"] = "0";
-        }
-    }
-
-    /// <summary>
-    /// The 'Product' class
-    /// </summary>
-
-    class Vehicle
-    {
-        private string _vehicleType;
-        private Dictionary<string, string> _parts =
-          new Dictionary<string, string>();
+        protected Receiver receiver;
 
         // Constructor
 
-        public Vehicle(string vehicleType)
+        public Command(Receiver receiver)
         {
-            this._vehicleType = vehicleType;
+            this.receiver = receiver;
         }
 
-        // Indexer
+        public abstract void Execute();
+    }
 
-        public string this[string key]
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+
+    public class ConcreteCommand : Command
+    {
+        // Constructor
+
+        public ConcreteCommand(Receiver receiver) :
+            base(receiver)
         {
-            get { return _parts[key]; }
-            set { _parts[key] = value; }
         }
 
-        public void Show()
+        public override void Execute()
         {
-            Console.WriteLine("\n---------------------------");
-            Console.WriteLine("Vehicle Type: {0}", _vehicleType);
-            Console.WriteLine(" Frame : {0}", _parts["frame"]);
-            Console.WriteLine(" Engine : {0}", _parts["engine"]);
-            Console.WriteLine(" #Wheels: {0}", _parts["wheels"]);
-            Console.WriteLine(" #Doors : {0}", _parts["doors"]);
+            receiver.Action();
         }
+    }
+
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+
+    public class Receiver
+    {
+        public void Action()
+        {
+            Console.WriteLine("Called Receiver.Action()");
+        }
+    }
+
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+
+    public class Invoker
+    {
+        Command command;
+
+        public void SetCommand(Command command)
+        {
+            this.command = command;
+        }
+
+        public void ExecuteCommand()
+        {
+            command.Execute();
+        }
+    }
+
+    /// <summary>
+    /// Command Design Pattern
+    /// </summary>
+
+    public class Program6
+    {
+        public static void Main(string[] args)
+        {
+            // Create user and let her compute
+
+            User user = new User();
+
+            // User presses calculator buttons
+
+            user.Compute('+', 100);
+            user.Compute('-', 50);
+            user.Compute('*', 10);
+            user.Compute('/', 2);
+
+            // Undo 4 commands
+
+            user.Undo(4);
+
+            // Redo 3 commands
+
+            user.Redo(3);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// The 'Command' abstract class
+    /// </summary>
+
+    public abstract class Command
+    {
+        public abstract void Execute();
+        public abstract void UnExecute();
+    }
+
+    /// <summary>
+    /// The 'ConcreteCommand' class
+    /// </summary>
+
+    public class CalculatorCommand : Command
+    {
+        char @operator;
+        int operand;
+        Calculator calculator;
+
+        // Constructor
+
+        public CalculatorCommand(Calculator calculator,
+            char @operator, int operand)
+        {
+            this.calculator = calculator;
+            this.@operator = @operator;
+            this.operand = operand;
+        }
+
+        // Gets operator
+
+        public char Operator
+        {
+            set { @operator = value; }
+        }
+
+        // Get operand
+
+        public int Operand
+        {
+            set { operand = value; }
+        }
+
+        // Execute new command
+
+        public override void Execute()
+        {
+            calculator.Operation(@operator, operand);
+        }
+
+        // Unexecute last command
+
+        public override void UnExecute()
+        {
+            calculator.Operation(Undo(@operator), operand);
+        }
+
+        // Returns opposite operator for given operator
+
+        private char Undo(char @operator)
+        {
+            switch (@operator)
+            {
+                case '+': return '-';
+                case '-': return '+';
+                case '*': return '/';
+                case '/': return '*';
+                default:
+                    throw new
+             ArgumentException("@operator");
+            }
+        }
+    }
+
+    /// <summary>
+    /// The 'Receiver' class
+    /// </summary>
+
+    public class Calculator
+    {
+        int curr = 0;
+
+        public void Operation(char @operator, int operand)
+        {
+            switch (@operator)
+            {
+                case '+': curr += operand; break;
+                case '-': curr -= operand; break;
+                case '*': curr *= operand; break;
+                case '/': curr /= operand; break;
+            }
+            Console.WriteLine(
+                "Current value = {0,3} (following {1} {2})",
+                curr, @operator, operand);
+        }
+    }
+
+    /// <summary>
+    /// The 'Invoker' class
+    /// </summary>
+
+    public class User
+    {
+        // Initializers
+
+        Calculator calculator = new Calculator();
+        List<Command> commands = new List<Command>();
+        int current = 0;
+
+        public void Redo(int levels)
+        {
+            Console.WriteLine("\n---- Redo {0} levels ", levels);
+            // Perform redo operations
+            for (int i = 0; i < levels; i++)
+            {
+                if (current < commands.Count - 1)
+                {
+                    Command command = commands[current++];
+                    command.Execute();
+                }
+            }
+        }
+
+        public void Undo(int levels)
+        {
+            Console.WriteLine("\n---- Undo {0} levels ", levels);
+
+            // Perform undo operations
+
+            for (int i = 0; i < levels; i++)
+            {
+                if (current > 0)
+                {
+                    Command command = commands[--current] as Command;
+                    command.UnExecute();
+                }
+            }
+        }
+
+        public void Compute(char @operator, int operand)
+        {
+            // Create command operation and execute it
+
+            Command command = new CalculatorCommand(calculator, @operator, operand);
+            command.Execute();
+
+            // Add command to undo list
+
+            commands.Add(command);
+            current++;
+        }
+
+
     }
 }
