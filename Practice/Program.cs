f2417af54de3aaf878e49a0b112aaf736897ead6
Sean Park <whiteibescu@gmail.com>
2022-05-18 19:33:25 +0900

    Factory

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,202 +1,82 @@
 ﻿using System;
-
-namespace Design_Pattern
+namespace Factory
 {
-
-    /// Structural code in C#
-    /// Facade Design Pattern
+    /// <summary>
+    /// The 'Product' interface
     /// </summary>
-
-    public class Program
+    public interface IFactory
     {
-        public static void Main(string[] args)
+        void Drive(int miles);
+    }
+
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class Scooter : IFactory
+    {
+        public void Drive(int miles)
         {
-            Facade facade = new Facade();
+            Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+        }
+    }
 
-            facade.MethodA();
-            facade.MethodB();
+    /// <summary>
+    /// A 'ConcreteProduct' class
+    /// </summary>
+    public class Bike : IFactory
+    {
+        public void Drive(int miles)
+        {
+            Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+        }
+    }
 
-            // Wait for user
+    /// <summary>
+    /// The Creator Abstract Class
+    /// </summary>
+    public abstract class VehicleFactory
+    {
+        public abstract IFactory GetVehicle(string Vehicle);
+
+    }
+
+    /// <summary>
+    /// A 'ConcreteCreator' class
+    /// </summary>
+    public class ConcreteVehicleFactory : VehicleFactory
+    {
+        public override IFactory GetVehicle(string Vehicle)
+        {
+            switch (Vehicle)
+            {
+                case "Scooter":
+                    return new Scooter();
+                case "Bike":
+                    return new Bike();
+                default:
+                    throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", Vehicle));
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Factory Pattern Demo
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VehicleFactory factory = new ConcreteVehicleFactory();
+
+            IFactory scooter = factory.GetVehicle("Scooter");
+            scooter.Drive(10);
+
+            IFactory bike = factory.GetVehicle("Bike");
+            bike.Drive(20);
 
             Console.ReadKey();
+
         }
     }
-
-    /// <summary>
-    /// The 'Subsystem ClassA' class
-    /// </summary>
-
-    public class SubSystemOne
-    {
-        public void MethodOne()
-        {
-            Console.WriteLine(" SubSystemOne Method");
-        }
-    }
-
-    /// <summary>
-    /// The 'Subsystem ClassB' class
-    /// </summary>
-
-    public class SubSystemTwo
-    {
-        public void MethodTwo()
-        {
-            Console.WriteLine(" SubSystemTwo Method");
-        }
-    }
-
-    /// <summary>
-    /// The 'Subsystem ClassC' class
-    /// </summary>
-
-    public class SubSystemThree
-    {
-        public void MethodThree()
-        {
-            Console.WriteLine(" SubSystemThree Method");
-        }
-    }
-
-    /// <summary>
-    /// The 'Subsystem ClassD' class
-    /// </summary>
-
-    public class SubSystemFour
-    {
-        public void MethodFour()
-        {
-            Console.WriteLine(" SubSystemFour Method");
-        }
-    }
-
-    /// <summary>
-    /// The 'Facade' class
-    /// </summary>
-
-    public class Facade
-    {
-        SubSystemOne one;
-        SubSystemTwo two;
-        SubSystemThree three;
-        SubSystemFour four;
-
-        public Facade()
-        {
-            one = new SubSystemOne();
-            two = new SubSystemTwo();
-            three = new SubSystemThree();
-            four = new SubSystemFour();
-        }
-
-        public void MethodA()
-        {
-            Console.WriteLine("\nMethodA() ---- ");
-            one.MethodOne();
-            two.MethodTwo();
-            four.MethodFour();
-        }
-
-        public void MethodB()
-        {
-            Console.WriteLine("\nMethodB() ---- ");
-            two.MethodTwo();
-            three.MethodThree();
-        }
-    }
-    public class Program2
-    {
-        public static void Main(string[] args)
-        {
-            // Facade
-            Mortgage mortgage = new Mortgage();
-            // Evaluate mortgage eligibility for customer
-            Customer customer = new Customer("Ann McKinsey");
-            bool eligible = mortgage.IsEligible(customer, 125000);
-            Console.WriteLine("\n" + customer.Name +
-                    " has been " + (eligible ? "Approved" : "Rejected"));
-            // Wait for user
-            Console.ReadKey();
-        }
-    }
-    /// <summary>
-    /// The 'Subsystem ClassA' class
-    /// </summary>
-    public class Bank
-    {
-        public bool HasSufficientSavings(Customer c, int amount)
-        {
-            Console.WriteLine("Check bank for " + c.Name);
-            return true;
-        }
-    }
-    /// <summary>
-    /// The 'Subsystem ClassB' class
-    /// </summary>
-    public class Credit
-    {
-        public bool HasGoodCredit(Customer c)
-        {
-            Console.WriteLine("Check credit for " + c.Name);
-            return true;
-        }
-    }
-    /// <summary>
-    /// The 'Subsystem ClassC' class
-    /// </summary>
-    public class Loan
-    {
-        public bool HasNoBadLoans(Customer c)
-        {
-            Console.WriteLine("Check loans for " + c.Name);
-            return true;
-        }
-    }
-    /// <summary>
-    /// Customer class
-    /// </summary>
-    public class Customer
-    {
-        private string name;
-        // Constructor
-        public Customer(string name)
-        {
-            this.name = name;
-        }
-        public string Name
-        {
-            get { return name; }
-        }
-    }
-
-    /// <summary>
-    /// The 'Facade' class
-    /// </summary>
-    public class Mortgage
-    {
-        Bank bank = new Bank();
-        Loan loan = new Loan();
-        Credit credit = new Credit();
-        public bool IsEligible(Customer cust, int amount)
-        {
-            Console.WriteLine("{0} applies for {1:C} loan\n",
-                cust.Name, amount);
-            bool eligible = true;
-            // Check creditworthyness of applicant
-            if (!bank.HasSufficientSavings(cust, amount))
-            {
-                eligible = false;
-            }
-            else if (!loan.HasNoBadLoans(cust))
-            {
-                eligible = false;
-            }
-            else if (!credit.HasGoodCredit(cust))
-            {
-                eligible = false;
-            }
-            return eligible;
-        }
-    }
-
 }
