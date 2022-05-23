39ce195a83629ea463e21c25d58385cff11f4b36
Sean Park <whiteibescu@gmail.com>
2022-05-23 11:52:05 +0900

    Momento

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,304 +1,90 @@
 ﻿using System;
 
-namespace Mediator.Structural
+
+namespace Memento.Structural
 {
     /// <summary>
-    /// Mediator Design Pattern
+    /// Memento Design Pattern
     /// </summary>
-
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            ConcreteMediator m = new ConcreteMediator();
+            Originator o = new Originator();
+            o.State = "On";
 
-            ConcreteColleague1 c1 = new ConcreteColleague1(m);
-            ConcreteColleague2 c2 = new ConcreteColleague2(m);
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
 
-            m.Colleague1 = c1;
-            m.Colleague2 = c2;
+            o.State = "Off";
 
-            c1.Send("How are you?");
-            c2.Send("Fine, thanks");
-
-            // Wait for user
+            o.SetMemento(c.Memento);
 
             Console.ReadKey();
+
         }
     }
 
     /// <summary>
-    /// The 'Mediator' abstract class
+    /// The 'Originator' class
     /// </summary>
 
-    public abstract class Mediator
+    public class Originator
     {
-        public abstract void Send(string message,
-            Colleague colleague);
-    }
+        string state;
 
-    /// <summary>
-    /// The 'ConcreteMediator' class
-    /// </summary>
-
-    public class ConcreteMediator : Mediator
-    {
-        ConcreteColleague1 colleague1;
-        ConcreteColleague2 colleague2;
-
-        public ConcreteColleague1 Colleague1
+        public string State
         {
-            set { colleague1 = value; }
-        }
-
-        public ConcreteColleague2 Colleague2
-        {
-            set { colleague2 = value; }
-        }
-
-        public override void Send(string message, Colleague colleague)
-        {
-            if (colleague == colleague1)
+            get { return state; }
+            set
             {
-                colleague2.Notify(message);
-            }
-            else
-            {
-                colleague1.Notify(message);
+                state = value;
+                Console.WriteLine("Satate = " + state);
             }
         }
-    }
 
-    /// <summary>
-    /// The 'Colleague' abstract class
-    /// </summary>
-
-    public abstract class Colleague
-    {
-        protected Mediator mediator;
-
-        // Constructor
-
-        public Colleague(Mediator mediator)
+        public Memento CreateMemento()
         {
-            this.mediator = mediator;
+            return (new Memento(state));
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
         }
     }
-
     /// <summary>
-    /// A 'ConcreteColleague' class
+    /// The 'Memento' class
     /// </summary>
-
-    public class ConcreteColleague1 : Colleague
+    public class Memento
     {
-        // Constructor
+        string state;
 
-        public ConcreteColleague1(Mediator mediator)
-            : base(mediator)
+        public Memento(string state)
         {
+            this.state = state;
         }
 
-        public void Send(string message)
+        public string State
         {
-            mediator.Send(message, this);
-        }
-
-        public void Notify(string message)
-        {
-            Console.WriteLine("Colleague1 gets message: "
-                + message);
+            get { return state; }
         }
     }
 
     /// <summary>
-    /// A 'ConcreteColleague' class
+    /// The 'Caretaker' class
     /// </summary>
 
-    public class ConcreteColleague2 : Colleague
+
+    public class Caretaker
     {
-        // Constructor
+        Memento memento;
 
-        public ConcreteColleague2(Mediator mediator)
-            : base(mediator)
+        public Memento Memento
         {
-        }
-
-        public void Send(string message)
-        {
-            mediator.Send(message, this);
-        }
-
-        public void Notify(string message)
-        {
-            Console.WriteLine("Colleague2 gets message: "
-                + message);
-        }
-    }
-
-    /// Real World
-    /// Mediator Design Pattern
-    /// </summary>
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Create chatroom
-
-            Chatroom chatroom = new Chatroom();
-
-            // Create participants and register them
-
-            Participant George = new Beatle("George");
-            Participant Paul = new Beatle("Paul");
-            Participant Ringo = new Beatle("Ringo");
-            Participant John = new Beatle("John");
-            Participant Yoko = new NonBeatle("Yoko");
-
-            chatroom.Register(George);
-            chatroom.Register(Paul);
-            chatroom.Register(Ringo);
-            chatroom.Register(John);
-            chatroom.Register(Yoko);
-
-            // Chatting participants
-
-            Yoko.Send("John", "Hi John!");
-            Paul.Send("Ringo", "All you need is love");
-            Ringo.Send("George", "My sweet Lord");
-            Paul.Send("John", "Can't buy me love");
-            John.Send("Yoko", "My sweet love");
-
-            // Wait for user
-
-            Console.ReadKey();
-        }
-    }
-
-    /// <summary>
-    /// The 'Mediator' abstract class
-    /// </summary>
-
-    public abstract class AbstractChatroom
-    {
-        public abstract void Register(Participant participant);
-        public abstract void Send(
-            string from, string to, string message);
-    }
-
-    /// <summary>
-    /// The 'ConcreteMediator' class
-    /// </summary>
-
-    public class Chatroom : AbstractChatroom
-    {
-        private Dictionary<string, Participant> participants = new Dictionary<string, Participant>();
-
-        public override void Register(Participant participant)
-        {
-            if (!participants.ContainsValue(participant))
-            {
-                participants[participant.Name] = participant;
-            }
-
-            participant.Chatroom = this;
-        }
-
-        public override void Send(string from, string to, string message)
-        {
-            Participant participant = participants[to];
-
-            if (participant != null)
-            {
-                participant.Receive(from, message);
-            }
-        }
-    }
-
-    /// <summary>
-    /// The 'AbstractColleague' class
-    /// </summary>
-
-    public class Participant
-    {
-        Chatroom chatroom;
-        string name;
-
-        // Constructor
-
-        public Participant(string name)
-        {
-            this.name = name;
-        }
-
-        // Gets participant name
-
-        public string Name
-        {
-            get { return name; }
-        }
-
-        // Gets chatroom
-
-        public Chatroom Chatroom
-        {
-            set { chatroom = value; }
-            get { return chatroom; }
-        }
-
-        // Sends message to given participant
-
-        public void Send(string to, string message)
-        {
-            chatroom.Send(name, to, message);
-        }
-
-        // Receives message from given participant
-
-        public virtual void Receive(
-            string from, string message)
-        {
-            Console.WriteLine("{0} to {1}: '{2}'",
-                from, Name, message);
-        }
-    }
-
-    /// <summary>
-    /// A 'ConcreteColleague' class
-    /// </summary>
-
-    public class Beatle : Participant
-    {
-        // Constructor
-
-        public Beatle(string name)
-            : base(name)
-        {
-        }
-
-        public override void Receive(string from, string message)
-        {
-            Console.Write("To a Beatle: ");
-            base.Receive(from, message);
-        }
-    }
-
-    /// <summary>
-    /// A 'ConcreteColleague' class
-    /// </summary>
-
-    public class NonBeatle : Participant
-    {
-        // Constructor
-        public NonBeatle(string name)
-            : base(name)
-        {
-        }
-
-        public override void Receive(string from, string message)
-        {
-            Console.Write("To a non-Beatle: ");
-            base.Receive(from, message);
+            set { memento = value; }
+            get { return memento; }
         }
     }
 }
