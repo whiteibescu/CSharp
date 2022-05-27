15ea6835947792a823efa14ca049f22daad90e2f
Sean Park <whiteibescu@gmail.com>
2022-05-27 23:15:53 +0900

    Observer

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,90 +1,121 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
-
-namespace Memento.Structural
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
 {
-    /// <summary>
-    /// Memento Design Pattern
-    /// </summary>
-    public class Program
+    public interface IObserver
     {
-        static void Main(string[] args)
-        {
-            Originator o = new Originator();
-            o.State = "On";
-
-            Caretaker c = new Caretaker();
-            c.Memento = o.CreateMemento();
-
-            o.State = "Off";
-
-            o.SetMemento(c.Memento);
-
-            Console.ReadKey();
-
-        }
+        // Receive update from subject
+        void Update(ISubject subject);
     }
 
-    /// <summary>
-    /// The 'Originator' class
-    /// </summary>
-
-    public class Originator
+    public interface ISubject
     {
-        string state;
+        // Attach an observer to the subject.
+        void Attach(IObserver observer);
 
-        public string State
+        // Detach an observer from the subject.
+        void Detach(IObserver observer);
+
+        // Notify all observers about an event.
+        void Notify();
+    }
+
+    // The Subject owns some important state and notifies observers when the
+    // state changes.
+    public class Subject : ISubject
+    {
+        // For the sake of simplicity, the Subject's state, essential to all
+        // subscribers, is stored in this variable.
+        public int State { get; set; } = -0;
+
+        // List of subscribers. In real life, the list of subscribers can be
+        // stored more comprehensively (categorized by event type, etc.).
+        private List<IObserver> _observers = new List<IObserver>();
+
+        // The subscription management methods.
+        public void Attach(IObserver observer)
         {
-            get { return state; }
-            set
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
+        }
+
+        // Trigger an update in each subscriber.
+        public void Notify()
+        {
+            Console.WriteLine("Subject: Notifying observers...");
+
+            foreach (var observer in _observers)
             {
-                state = value;
-                Console.WriteLine("Satate = " + state);
+                observer.Update(this);
             }
         }
 
-        public Memento CreateMemento()
+        // Usually, the subscription logic is only a fraction of what a Subject
+        // can really do. Subjects commonly hold some important business logic,
+        // that triggers a notification method whenever something important is
+        // about to happen (or after it).
+        public void SomeBusinessLogic()
         {
-            return (new Memento(state));
-        }
+            Console.WriteLine("\nSubject: I'm doing something important.");
+            this.State = new Random().Next(0, 10);
 
-        public void SetMemento(Memento memento)
-        {
-            Console.WriteLine("Restoring state...");
-            State = memento.State;
-        }
-    }
-    /// <summary>
-    /// The 'Memento' class
-    /// </summary>
-    public class Memento
-    {
-        string state;
+            Thread.Sleep(15);
 
-        public Memento(string state)
-        {
-            this.state = state;
-        }
-
-        public string State
-        {
-            get { return state; }
+            Console.WriteLine("Subject: My state has just changed to: " + this.State);
+            this.Notify();
         }
     }
 
-    /// <summary>
-    /// The 'Caretaker' class
-    /// </summary>
-
-
-    public class Caretaker
+    // Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    class ConcreteObserverA : IObserver
     {
-        Memento memento;
-
-        public Memento Memento
+        public void Update(ISubject subject)
         {
-            set { memento = value; }
-            get { return memento; }
+            if ((subject as Subject).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+            }
+        }
+    }
+
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
         }
     }
 }
