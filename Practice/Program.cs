f7815ee6446b29d67c34df9ae1a61ee24b25e95a
Sean Park <whiteibescu@gmail.com>
2022-06-21 22:27:44 +0900

    Prototype

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,171 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Decorator.RealWorld
+namespace Prototype.RealWorld
 {
     /// <summary>
-    /// Decorator Design Pattern
+    /// Prototype Design Pattern
     /// </summary>
 
     public class Program
     {
         public static void Main(string[] args)
         {
-            // Create book
+            ColorManager colormanager = new ColorManager();
 
-            Book book = new Book("Worley", "Inside ASP.NET", 10);
-            book.Display();
+            // Initialize with standard colors
 
-            // Create video
+            colormanager["red"] = new Color(255, 0, 0);
+            colormanager["green"] = new Color(0, 255, 0);
+            colormanager["blue"] = new Color(0, 0, 255);
 
-            Video video = new Video("Spielberg", "Jaws", 23, 92);
-            video.Display();
+            // User adds personalized colors
 
-            // Make video borrowable, then borrow and display
+            colormanager["angry"] = new Color(255, 54, 0);
+            colormanager["peace"] = new Color(128, 211, 128);
+            colormanager["flame"] = new Color(211, 34, 20);
 
-            Console.WriteLine("\nMaking video borrowable:");
+            // User clones selected colors
 
-            Borrowable borrowvideo = new Borrowable(video);
-            borrowvideo.BorrowItem("Customer #1");
-            borrowvideo.BorrowItem("Customer #2");
-
-            borrowvideo.Display();
+            Color color1 = colormanager["red"].Clone() as Color;
+            Color color2 = colormanager["peace"].Clone() as Color;
+            Color color3 = colormanager["flame"].Clone() as Color;
 
             // Wait for user
 
             Console.ReadKey();
         }
     }
+
     /// <summary>
-    /// The 'Component' abstract class
+    /// The 'Prototype' abstract class
     /// </summary>
 
-    public abstract class LibraryItem
+    public abstract class ColorPrototype
     {
-        private int numCopies;
-
-        public int NumCopies
-        {
-            get { return numCopies; }
-            set { numCopies = value; }
-        }
-
-        public abstract void Display();
+        public abstract ColorPrototype Clone();
     }
 
     /// <summary>
-    /// The 'ConcreteComponent' class
+    /// The 'ConcretePrototype' class
     /// </summary>
 
-    public class Book : LibraryItem
+    public class Color : ColorPrototype
     {
-        private string author;
-        private string title;
+        int red;
+        int green;
+        int blue;
 
         // Constructor
 
-        public Book(string author, string title, int numCopies)
+        public Color(int red, int green, int blue)
         {
-            this.author = author;
-            this.title = title;
-            this.NumCopies = numCopies;
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
         }
 
-        public override void Display()
+        // Create a shallow copy
+
+        public override ColorPrototype Clone()
         {
-            Console.WriteLine("\nBook ------ ");
-            Console.WriteLine(" Author: {0}", author);
-            Console.WriteLine(" Title: {0}", title);
-            Console.WriteLine(" # Copies: {0}", NumCopies);
-        }
-    }
+            Console.WriteLine(
+                "Cloning color RGB: {0,3},{1,3},{2,3}",
+                red, green, blue);
 
-    /// <summary>
-    /// The 'ConcreteComponent' class
-    /// </summary>
-
-    public class Video : LibraryItem
-    {
-        private string director;
-        private string title;
-        private int playTime;
-
-        // Constructor
-
-        public Video(string director, string title, int numCopies, int playTime)
-        {
-            this.director = director;
-            this.title = title;
-            this.NumCopies = numCopies;
-            this.playTime = playTime;
-        }
-
-        public override void Display()
-        {
-            Console.WriteLine("\nVideo ----- ");
-            Console.WriteLine(" Director: {0}", director);
-            Console.WriteLine(" Title: {0}", title);
-            Console.WriteLine(" # Copies: {0}", NumCopies);
-            Console.WriteLine(" Playtime: {0}\n", playTime);
+            return this.MemberwiseClone() as ColorPrototype;
         }
     }
 
     /// <summary>
-    /// The 'Decorator' abstract class
+    /// Prototype manager
     /// </summary>
 
-    public abstract class Decorator : LibraryItem
+    public class ColorManager
     {
-        protected LibraryItem libraryItem;
+        private Dictionary<string, ColorPrototype> colors =
+            new Dictionary<string, ColorPrototype>();
 
-        // Constructor
+        // Indexer
 
-        public Decorator(LibraryItem libraryItem)
+        public ColorPrototype this[string key]
         {
-            this.libraryItem = libraryItem;
-        }
-
-        public override void Display()
-        {
-            libraryItem.Display();
-        }
-    }
-
-    /// <summary>
-    /// The 'ConcreteDecorator' class
-    /// </summary>
-
-    public class Borrowable : Decorator
-    {
-        protected readonly List<string> borrowers = new List<string>();
-
-        // Constructor
-
-        public Borrowable(LibraryItem libraryItem)
-            : base(libraryItem)
-        {
-        }
-
-        public void BorrowItem(string name)
-        {
-            borrowers.Add(name);
-            libraryItem.NumCopies--;
-        }
-
-        public void ReturnItem(string name)
-        {
-            borrowers.Remove(name);
-            libraryItem.NumCopies++;
-        }
-
-        public override void Display()
-        {
-            base.Display();
-
-            foreach (string borrower in borrowers)
-            {
-                Console.WriteLine(" borrower: " + borrower);
-            }
+            get { return colors[key]; }
+            set { colors.Add(key, value); }
         }
     }
 }
