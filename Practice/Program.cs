789a23d815b0f211010c84bbe3208ac2e81a4ca6
Sean Park <whiteibescu@gmail.com>
2022-06-29 20:11:54 +0900

    Momento

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,266 +1,90 @@
 ﻿using System;
 
-namespace Chain.Structural
+
+namespace Memento.Structural
 {
     /// <summary>
-    /// Chain of Responsibility Design Pattern
+    /// Memento Design Pattern
     /// </summary>
-
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            // Setup Chain of Responsibility
+            Originator o = new Originator();
+            o.State = "On";
 
-            Handler h1 = new ConcreteHandler1();
-            Handler h2 = new ConcreteHandler2();
-            Handler h3 = new ConcreteHandler3();
-            h1.SetSuccessor(h2);
-            h2.SetSuccessor(h3);
+            Caretaker c = new Caretaker();
+            c.Memento = o.CreateMemento();
 
-            // Generate and process request
+            o.State = "Off";
 
-            int[] requests = { 2, 5, 14, 22, 18, 3, 27, 20 };
-
-            foreach (int request in requests)
-            {
-                h1.HandleRequest(request);
-            }
-
-            // Wait for user
+            o.SetMemento(c.Memento);
 
             Console.ReadKey();
-        }
-    }
-    /// <summary>
-    /// The 'Handler' abstract class
-    /// </summary>
 
-    public abstract class Handler
-    {
-        protected Handler successor;
-
-        public void SetSuccessor(Handler successor)
-        {
-            this.successor = successor;
-        }
-
-        public abstract void HandleRequest(int request);
-    }
-
-    /// <summary>
-    /// The 'ConcreteHandler1' class
-    /// </summary>
-
-    public class ConcreteHandler1 : Handler
-    {
-        public override void HandleRequest(int request)
-        {
-            if (request >= 0 && request < 10)
-            {
-                Console.WriteLine("{0} handled request {1}",
-                    this.GetType().Name, request);
-            }
-            else if (successor != null)
-            {
-                successor.HandleRequest(request);
-            }
         }
     }
 
     /// <summary>
-    /// The 'ConcreteHandler2' class
+    /// The 'Originator' class
     /// </summary>
 
-    public class ConcreteHandler2 : Handler
+    public class Originator
     {
-        public override void HandleRequest(int request)
+        string state;
+
+        public string State
         {
-            if (request >= 10 && request < 20)
+            get { return state; }
+            set
             {
-                Console.WriteLine("{0} handled request {1}",
-                    this.GetType().Name, request);
+                state = value;
+                Console.WriteLine("Satate = " + state);
             }
-            else if (successor != null)
-            {
-                successor.HandleRequest(request);
-            }
+        }
+
+        public Memento CreateMemento()
+        {
+            return (new Memento(state));
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            Console.WriteLine("Restoring state...");
+            State = memento.State;
+        }
+    }
+    /// <summary>
+    /// The 'Memento' class
+    /// </summary>
+    public class Memento
+    {
+        string state;
+
+        public Memento(string state)
+        {
+            this.state = state;
+        }
+
+        public string State
+        {
+            get { return state; }
         }
     }
 
     /// <summary>
-    /// The 'ConcreteHandler3' class
+    /// The 'Caretaker' class
     /// </summary>
 
-    public class ConcreteHandler3 : Handler
+
+    public class Caretaker
     {
-        public override void HandleRequest(int request)
+        Memento memento;
+
+        public Memento Memento
         {
-            if (request >= 20 && request < 30)
-            {
-                Console.WriteLine("{0} handled request {1}",
-                    this.GetType().Name, request);
-            }
-            else if (successor != null)
-            {
-                successor.HandleRequest(request);
-            }
+            set { memento = value; }
+            get { return memento; }
         }
     }
-
-    /// <summary>
-    /// Chain of Responsibility Design Pattern
-    /// </summary>
-
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            // Setup Chain of Responsibility
-
-            Approver larry = new Director();
-            Approver sam = new VicePresident();
-            Approver tammy = new President();
-
-            larry.SetSuccessor(sam);
-            sam.SetSuccessor(tammy);
-
-            // Generate and process purchase requests
-
-            Purchase p = new Purchase(2034, 350.00, "Supplies");
-            larry.ProcessRequest(p);
-
-            p = new Purchase(2035, 32590.10, "Project X");
-            larry.ProcessRequest(p);
-
-            p = new Purchase(2036, 122100.00, "Project Y");
-            larry.ProcessRequest(p);
-
-            // Wait for user
-
-            Console.ReadKey();
-        }
-    }
-    /// <summary>
-    /// The 'Handler' abstract class
-    /// </summary>
-
-    public abstract class Approver
-    {
-        protected Approver successor;
-
-        public void SetSuccessor(Approver successor)
-        {
-            this.successor = successor;
-        }
-
-        public abstract void ProcessRequest(Purchase purchase);
-    }
-
-    /// <summary>
-    /// The 'ConcreteHandler' class
-    /// </summary>
-
-    public class Director : Approver
-    {
-        public override void ProcessRequest(Purchase purchase)
-        {
-            if (purchase.Amount < 10000.0)
-            {
-                Console.WriteLine("{0} approved request# {1}",
-                    this.GetType().Name, purchase.Number);
-            }
-            else if (successor != null)
-            {
-                successor.ProcessRequest(purchase);
-            }
-        }
-    }
-
-    /// <summary>
-    /// The 'ConcreteHandler' class
-    /// </summary>
-
-    public class VicePresident : Approver
-    {
-        public override void ProcessRequest(Purchase purchase)
-        {
-            if (purchase.Amount < 25000.0)
-            {
-                Console.WriteLine("{0} approved request# {1}",
-                    this.GetType().Name, purchase.Number);
-            }
-            else if (successor != null)
-            {
-                successor.ProcessRequest(purchase);
-            }
-        }
-    }
-
-    /// <summary>
-    /// The 'ConcreteHandler' class
-    /// </summary>
-
-    public class President : Approver
-    {
-        public override void ProcessRequest(Purchase purchase)
-        {
-            if (purchase.Amount < 100000.0)
-            {
-                Console.WriteLine("{0} approved request# {1}",
-                    this.GetType().Name, purchase.Number);
-            }
-            else
-            {
-                Console.WriteLine(
-                    "Request# {0} requires an executive meeting!",
-                    purchase.Number);
-            }
-        }
-    }
-
-    /// Real Live
-    /// Class holding request details
-    /// </summary>
-
-    public class Purchase
-    {
-        int number;
-        double amount;
-        string purpose;
-
-        // Constructor
-
-        public Purchase(int number, double amount, string purpose)
-        {
-            this.number = number;
-            this.amount = amount;
-            this.purpose = purpose;
-        }
-
-        // Gets or sets purchase number
-
-        public int Number
-        {
-            get { return number; }
-            set { number = value; }
-        }
-
-        // Gets or sets purchase amount
-
-        public double Amount
-        {
-            get { return amount; }
-            set { amount = value; }
-        }
-
-        // Gets or sets purchase purpose
-
-        public string Purpose
-        {
-            get { return purpose; }
-            set { purpose = value; }
-        }
-    }
-
 }
