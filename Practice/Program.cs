da380233d4ae70b3b93605affa42eab813d7880d
Sean Park <whiteibescu@gmail.com>
2022-07-14 11:45:21 +0900

    Observer

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,82 +1,121 @@
 ﻿using System;
-namespace Factory
+using System.Collections.Generic;
+using System.Threading;
+
+namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
 {
-    /// <summary>
-    /// The 'Product' interface
-    /// </summary>
-    public interface IFactory
+    public interface IObserver
     {
-        void Drive(int miles);
+        // Receive update from subject
+        void Update(ISubject subject);
     }
 
-    /// <summary>
-    /// A 'ConcreteProduct' class
-    /// </summary>
-    public class Scooter : IFactory
+    public interface ISubject
     {
-        public void Drive(int miles)
+        // Attach an observer to the subject.
+        void Attach(IObserver observer);
+
+        // Detach an observer from the subject.
+        void Detach(IObserver observer);
+
+        // Notify all observers about an event.
+        void Notify();
+    }
+
+    // The Subject owns some important state and notifies observers when the
+    // state changes.
+    public class Subject : ISubject
+    {
+        // For the sake of simplicity, the Subject's state, essential to all
+        // subscribers, is stored in this variable.
+        public int State { get; set; } = -0;
+
+        // List of subscribers. In real life, the list of subscribers can be
+        // stored more comprehensively (categorized by event type, etc.).
+        private List<IObserver> _observers = new List<IObserver>();
+
+        // The subscription management methods.
+        public void Attach(IObserver observer)
         {
-            Console.WriteLine("Drive the Scooter : " + miles.ToString() + "km");
+            Console.WriteLine("Subject: Attached an observer.");
+            this._observers.Add(observer);
         }
-    }
 
-    /// <summary>
-    /// A 'ConcreteProduct' class
-    /// </summary>
-    public class Bike : IFactory
-    {
-        public void Drive(int miles)
+        public void Detach(IObserver observer)
         {
-            Console.WriteLine("Drive the Bike : " + miles.ToString() + "km");
+            this._observers.Remove(observer);
+            Console.WriteLine("Subject: Detached an observer.");
         }
-    }
 
-    /// <summary>
-    /// The Creator Abstract Class
-    /// </summary>
-    public abstract class VehicleFactory
-    {
-        public abstract IFactory GetVehicle(string Vehicle);
-
-    }
-
-    /// <summary>
-    /// A 'ConcreteCreator' class
-    /// </summary>
-    public class ConcreteVehicleFactory : VehicleFactory
-    {
-        public override IFactory GetVehicle(string Vehicle)
+        // Trigger an update in each subscriber.
+        public void Notify()
         {
-            switch (Vehicle)
+            Console.WriteLine("Subject: Notifying observers...");
+
+            foreach (var observer in _observers)
             {
-                case "Scooter":
-                    return new Scooter();
-                case "Bike":
-                    return new Bike();
-                default:
-                    throw new ApplicationException(string.Format("Vehicle '{0}' cannot be created", Vehicle));
+                observer.Update(this);
             }
         }
 
+        // Usually, the subscription logic is only a fraction of what a Subject
+        // can really do. Subjects commonly hold some important business logic,
+        // that triggers a notification method whenever something important is
+        // about to happen (or after it).
+        public void SomeBusinessLogic()
+        {
+            Console.WriteLine("\nSubject: I'm doing something important.");
+            this.State = new Random().Next(0, 10);
+
+            Thread.Sleep(15);
+
+            Console.WriteLine("Subject: My state has just changed to: " + this.State);
+            this.Notify();
+        }
     }
 
-    /// <summary>
-    /// Factory Pattern Demo
-    /// </summary>
+    // Concrete Observers react to the updates issued by the Subject they had
+    // been attached to.
+    class ConcreteObserverA : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State < 3)
+            {
+                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
+            }
+        }
+    }
+
+    class ConcreteObserverB : IObserver
+    {
+        public void Update(ISubject subject)
+        {
+            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
+            {
+                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
+            }
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            VehicleFactory factory = new ConcreteVehicleFactory();
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
 
-            IFactory scooter = factory.GetVehicle("Scooter");
-            scooter.Drive(10);
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
 
-            IFactory bike = factory.GetVehicle("Bike");
-            bike.Drive(20);
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
 
-            Console.ReadKey();
+            subject.Detach(observerB);
 
+            subject.SomeBusinessLogic();
         }
     }
 }
