ec8d227c8258a553fe30923951ed72b2106aabb8
Sean Park <whiteibescu@gmail.com>
2022-08-08 16:23:44 +0900

    Iterator

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,90 +1,312 @@
 ﻿using System;
+using System.Collections.Generic;
 
-
-namespace Memento.Structural
+namespace Iterator.Structural
 {
     /// <summary>
-    /// Memento Design Pattern
+    /// Iterator Design Pattern
     /// </summary>
+
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Originator o = new Originator();
-            o.State = "On";
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
 
-            Caretaker c = new Caretaker();
-            c.Memento = o.CreateMemento();
+            // Create Iterator and provide aggregate
 
-            o.State = "Off";
+            Iterator i = a.CreateIterator();
 
-            o.SetMemento(c.Memento);
+            Console.WriteLine("Iterating over collection:");
+
+            object item = i.First();
+
+            while (item != null)
+            {
+                Console.WriteLine(item);
+                item = i.Next();
+            }
+
+            // Wait for user
 
             Console.ReadKey();
-
         }
     }
 
     /// <summary>
-    /// The 'Originator' class
+    /// The 'Aggregate' abstract class
     /// </summary>
 
-    public class Originator
+    public abstract class Aggregate
     {
-        string state;
+        public abstract Iterator CreateIterator();
+    }
 
-        public string State
+    /// <summary>
+    /// The 'ConcreteAggregate' class
+    /// </summary>
+
+    public class ConcreteAggregate : Aggregate
+    {
+        List<object> items = new List<object>();
+
+        public override Iterator CreateIterator()
         {
-            get { return state; }
-            set
+            return new ConcreteIterator(this);
+        }
+
+        // Get item count
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        // Indexer
+
+        public object this[int index]
+        {
+            get { return items[index]; }
+            set { items.Insert(index, value); }
+        }
+    }
+
+    /// <summary>
+    /// The 'Iterator' abstract class
+    /// </summary>
+
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract object Next();
+        public abstract bool IsDone();
+        public abstract object CurrentItem();
+    }
+
+    /// <summary>
+    /// The 'ConcreteIterator' class
+    /// </summary>
+
+    public class ConcreteIterator : Iterator
+    {
+        ConcreteAggregate aggregate;
+        int current = 0;
+
+        // Constructor
+
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+
+        // Gets first iteration item
+
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        // Gets next iteration item
+
+        public override object Next()
+        {
+            object ret = null;
+            if (current < aggregate.Count - 1)
             {
-                state = value;
-                Console.WriteLine("Satate = " + state);
+                ret = aggregate[++current];
             }
+
+            return ret;
         }
 
-        public Memento CreateMemento()
+        // Gets current iteration item
+
+        public override object CurrentItem()
         {
-            return (new Memento(state));
+            return aggregate[current];
         }
 
-        public void SetMemento(Memento memento)
-        {
-            Console.WriteLine("Restoring state...");
-            State = memento.State;
-        }
-    }
-    /// <summary>
-    /// The 'Memento' class
-    /// </summary>
-    public class Memento
-    {
-        string state;
+        // Gets whether iterations are complete
 
-        public Memento(string state)
+        public override bool IsDone()
         {
-            this.state = state;
-        }
-
-        public string State
-        {
-            get { return state; }
+            return current >= aggregate.Count;
         }
     }
 
-    /// <summary>
-    /// The 'Caretaker' class
+    /// Real World
+    /// Iterator Design Pattern
     /// </summary>
 
-
-    public class Caretaker
+    public class Program
     {
-        Memento memento;
-
-        public Memento Memento
+        public static void Main(string[] args)
         {
-            set { memento = value; }
-            get { return memento; }
+            // Build a collection
+
+            Collection collection = new Collection();
+            collection[0] = new Item("Item 0");
+            collection[1] = new Item("Item 1");
+            collection[2] = new Item("Item 2");
+            collection[3] = new Item("Item 3");
+            collection[4] = new Item("Item 4");
+            collection[5] = new Item("Item 5");
+            collection[6] = new Item("Item 6");
+            collection[7] = new Item("Item 7");
+            collection[8] = new Item("Item 8");
+
+            // Create iterator
+
+            Iterator iterator = collection.CreateIterator();
+
+            // Skip every other item
+
+            iterator.Step = 2;
+
+            Console.WriteLine("Iterating over collection:");
+
+            for (Item item = iterator.First();
+                !iterator.IsDone; item = iterator.Next())
+            {
+                Console.WriteLine(item.Name);
+            }
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// A collection item
+    /// </summary>
+
+    public class Item
+    {
+        string name;
+
+        // Constructor
+
+        public Item(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+    }
+
+    /// <summary>
+    /// The 'Aggregate' interface
+    /// </summary>
+
+    public interface IAbstractCollection
+    {
+        Iterator CreateIterator();
+    }
+
+    /// <summary>
+    /// The 'ConcreteAggregate' class
+    /// </summary>
+
+    public class Collection : IAbstractCollection
+    {
+        List<Item> items = new List<Item>();
+
+        public Iterator CreateIterator()
+        {
+            return new Iterator(this);
+        }
+
+        // Gets item count
+
+        public int Count
+        {
+            get { return items.Count; }
+        }
+
+        // Indexer
+
+        public Item this[int index]
+        {
+            get { return items[index]; }
+            set { items.Add(value); }
+        }
+    }
+
+    /// <summary>
+    /// The 'Iterator' interface
+    /// </summary>
+
+    public interface IAbstractIterator
+    {
+        Item First();
+        Item Next();
+        bool IsDone { get; }
+        Item CurrentItem { get; }
+    }
+
+    /// <summary>
+    /// The 'ConcreteIterator' class
+    /// </summary>
+
+    public class Iterator : IAbstractIterator
+    {
+        Collection collection;
+        int current = 0;
+        int step = 1;
+
+        // Constructor
+
+        public Iterator(Collection collection)
+        {
+            this.collection = collection;
+        }
+
+        // Gets first item
+
+        public Item First()
+        {
+            current = 0;
+            return collection[current] as Item;
+        }
+
+        // Gets next item
+
+        public Item Next()
+        {
+            current += step;
+            if (!IsDone)
+                return collection[current] as Item;
+            else
+                return null;
+        }
+
+        // Gets or sets stepsize
+
+        public int Step
+        {
+            get { return step; }
+            set { step = value; }
+        }
+
+        // Gets current iterator item
+
+        public Item CurrentItem
+        {
+            get { return collection[current] as Item; }
+        }
+
+        // Gets whether iteration is complete
+
+        public bool IsDone
+        {
+            get { return current >= collection.Count; }
         }
     }
 }
