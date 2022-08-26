a26b790c2ec33fc8af45aec68cae33a3fb103459
Sean Park <whiteibescu@gmail.com>
2022-08-26 22:17:33 +0900

    Decorator

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,121 +1,171 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Threading;
 
-namespace RefactoringGuru.DesignPatterns.Observer.Conceptual
+namespace Decorator.RealWorld
 {
-    public interface IObserver
+    /// <summary>
+    /// Decorator Design Pattern
+    /// </summary>
+
+    public class Program
     {
-        // Receive update from subject
-        void Update(ISubject subject);
-    }
-
-    public interface ISubject
-    {
-        // Attach an observer to the subject.
-        void Attach(IObserver observer);
-
-        // Detach an observer from the subject.
-        void Detach(IObserver observer);
-
-        // Notify all observers about an event.
-        void Notify();
-    }
-
-    // The Subject owns some important state and notifies observers when the
-    // state changes.
-    public class Subject : ISubject
-    {
-        // For the sake of simplicity, the Subject's state, essential to all
-        // subscribers, is stored in this variable.
-        public int State { get; set; } = -0;
-
-        // List of subscribers. In real life, the list of subscribers can be
-        // stored more comprehensively (categorized by event type, etc.).
-        private List<IObserver> _observers = new List<IObserver>();
-
-        // The subscription management methods.
-        public void Attach(IObserver observer)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Subject: Attached an observer.");
-            this._observers.Add(observer);
+            // Create book
+
+            Book book = new Book("Worley", "Inside ASP.NET", 10);
+            book.Display();
+
+            // Create video
+
+            Video video = new Video("Spielberg", "Jaws", 23, 92);
+            video.Display();
+
+            // Make video borrowable, then borrow and display
+
+            Console.WriteLine("\nMaking video borrowable:");
+
+            Borrowable borrowvideo = new Borrowable(video);
+            borrowvideo.BorrowItem("Customer #1");
+            borrowvideo.BorrowItem("Customer #2");
+
+            borrowvideo.Display();
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+    /// <summary>
+    /// The 'Component' abstract class
+    /// </summary>
+
+    public abstract class LibraryItem
+    {
+        private int numCopies;
+
+        public int NumCopies
+        {
+            get { return numCopies; }
+            set { numCopies = value; }
         }
 
-        public void Detach(IObserver observer)
+        public abstract void Display();
+    }
+
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+
+    public class Book : LibraryItem
+    {
+        private string author;
+        private string title;
+
+        // Constructor
+
+        public Book(string author, string title, int numCopies)
         {
-            this._observers.Remove(observer);
-            Console.WriteLine("Subject: Detached an observer.");
+            this.author = author;
+            this.title = title;
+            this.NumCopies = numCopies;
         }
 
-        // Trigger an update in each subscriber.
-        public void Notify()
+        public override void Display()
         {
-            Console.WriteLine("Subject: Notifying observers...");
+            Console.WriteLine("\nBook ------ ");
+            Console.WriteLine(" Author: {0}", author);
+            Console.WriteLine(" Title: {0}", title);
+            Console.WriteLine(" # Copies: {0}", NumCopies);
+        }
+    }
 
-            foreach (var observer in _observers)
+    /// <summary>
+    /// The 'ConcreteComponent' class
+    /// </summary>
+
+    public class Video : LibraryItem
+    {
+        private string director;
+        private string title;
+        private int playTime;
+
+        // Constructor
+
+        public Video(string director, string title, int numCopies, int playTime)
+        {
+            this.director = director;
+            this.title = title;
+            this.NumCopies = numCopies;
+            this.playTime = playTime;
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine("\nVideo ----- ");
+            Console.WriteLine(" Director: {0}", director);
+            Console.WriteLine(" Title: {0}", title);
+            Console.WriteLine(" # Copies: {0}", NumCopies);
+            Console.WriteLine(" Playtime: {0}\n", playTime);
+        }
+    }
+
+    /// <summary>
+    /// The 'Decorator' abstract class
+    /// </summary>
+
+    public abstract class Decorator : LibraryItem
+    {
+        protected LibraryItem libraryItem;
+
+        // Constructor
+
+        public Decorator(LibraryItem libraryItem)
+        {
+            this.libraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            libraryItem.Display();
+        }
+    }
+
+    /// <summary>
+    /// The 'ConcreteDecorator' class
+    /// </summary>
+
+    public class Borrowable : Decorator
+    {
+        protected readonly List<string> borrowers = new List<string>();
+
+        // Constructor
+
+        public Borrowable(LibraryItem libraryItem)
+            : base(libraryItem)
+        {
+        }
+
+        public void BorrowItem(string name)
+        {
+            borrowers.Add(name);
+            libraryItem.NumCopies--;
+        }
+
+        public void ReturnItem(string name)
+        {
+            borrowers.Remove(name);
+            libraryItem.NumCopies++;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+
+            foreach (string borrower in borrowers)
             {
-                observer.Update(this);
+                Console.WriteLine(" borrower: " + borrower);
             }
-        }
-
-        // Usually, the subscription logic is only a fraction of what a Subject
-        // can really do. Subjects commonly hold some important business logic,
-        // that triggers a notification method whenever something important is
-        // about to happen (or after it).
-        public void SomeBusinessLogic()
-        {
-            Console.WriteLine("\nSubject: I'm doing something important.");
-            this.State = new Random().Next(0, 10);
-
-            Thread.Sleep(15);
-
-            Console.WriteLine("Subject: My state has just changed to: " + this.State);
-            this.Notify();
-        }
-    }
-
-    // Concrete Observers react to the updates issued by the Subject they had
-    // been attached to.
-    class ConcreteObserverA : IObserver
-    {
-        public void Update(ISubject subject)
-        {
-            if ((subject as Subject).State < 3)
-            {
-                Console.WriteLine("ConcreteObserverA: Reacted to the event.");
-            }
-        }
-    }
-
-    class ConcreteObserverB : IObserver
-    {
-        public void Update(ISubject subject)
-        {
-            if ((subject as Subject).State == 0 || (subject as Subject).State >= 2)
-            {
-                Console.WriteLine("ConcreteObserverB: Reacted to the event.");
-            }
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // The client code.
-            var subject = new Subject();
-            var observerA = new ConcreteObserverA();
-            subject.Attach(observerA);
-
-            var observerB = new ConcreteObserverB();
-            subject.Attach(observerB);
-
-            subject.SomeBusinessLogic();
-            subject.SomeBusinessLogic();
-
-            subject.Detach(observerB);
-
-            subject.SomeBusinessLogic();
         }
     }
 }
