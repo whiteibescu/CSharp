ebf44178cefea3ed585f5a69bcd28aaa69fe37ae
Sean Park <whiteibescu@gmail.com>
2022-09-13 23:01:25 +0900

    Observer

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,191 +1,225 @@
 ﻿using System;
 using System.Collections.Generic;
 
-namespace Visitor.RealWorld
+public class BaggageInfo
 {
-    /// <summary>
-    /// Visitor Design Pattern
-    /// </summary>
+    private int flightNo;
+    private string origin;
+    private int location;
 
-    public class Program
+    internal BaggageInfo(int flight, string from, int carousel)
     {
-        public static void Main(string[] args)
-        {
-            // Setup employee collection
-
-            Employees employee = new Employees();
-            employee.Attach(new Clerk());
-            employee.Attach(new Director());
-            employee.Attach(new President());
-
-            // Employees are 'visited'
-
-            employee.Accept(new IncomeVisitor());
-            employee.Accept(new VacationVisitor());
-
-            // Wait for user
-
-            Console.ReadKey();
-        }
+        this.flightNo = flight;
+        this.origin = from;
+        this.location = carousel;
     }
 
-    /// <summary>
-    /// The 'Visitor' interface
-    /// </summary>
-
-    public interface IVisitor
+    public int FlightNumber
     {
-        void Visit(Element element);
+        get { return this.flightNo; }
     }
 
-    /// <summary>
-    /// A 'ConcreteVisitor' class
-    /// </summary>
-
-    public class IncomeVisitor : IVisitor
+    public string From
     {
-        public void Visit(Element element)
-        {
-            Employee employee = element as Employee;
-
-            // Provide 10% pay raise
-
-            employee.Income *= 1.10;
-
-            Console.WriteLine("{0} {1}'s new income: {2:C}",
-                employee.GetType().Name, employee.Name,
-                employee.Income);
-        }
+        get { return this.origin; }
     }
 
-    /// <summary>
-    /// A 'ConcreteVisitor' class
-    /// </summary>
-
-    public class VacationVisitor : IVisitor
+    public int Carousel
     {
-        public void Visit(Element element)
-        {
-            Employee employee = element as Employee;
+        get { return this.location; }
+    }
+}
 
-            // Provide 3 extra vacation days
+public class BaggageHandler : IObservable<BaggageInfo>
+{
+    private List<IObserver<BaggageInfo>> observers;
+    private List<BaggageInfo> flights;
 
-            employee.VacationDays += 3;
-
-            Console.WriteLine("{0} {1}'s new vacation days: {2}",
-                employee.GetType().Name, employee.Name,
-                employee.VacationDays);
-        }
+    public BaggageHandler()
+    {
+        observers = new List<IObserver<BaggageInfo>>();
+        flights = new List<BaggageInfo>();
     }
 
-    /// <summary>
-    /// The 'Element' abstract class
-    /// </summary>
-
-    public abstract class Element
+    public IDisposable Subscribe(IObserver<BaggageInfo> observer)
     {
-        public abstract void Accept(IVisitor visitor);
+        // Check whether observer is already registered. If not, add it
+        if (!observers.Contains(observer))
+        {
+            observers.Add(observer);
+            // Provide observer with existing data.
+            foreach (var item in flights)
+                observer.OnNext(item);
+        }
+        return new Unsubscriber<BaggageInfo>(observers, observer);
     }
 
-    /// <summary>
-    /// The 'ConcreteElement' class
-    /// </summary>
-
-    public class Employee : Element
+    // Called to indicate all baggage is now unloaded.
+    public void BaggageStatus(int flightNo)
     {
-        private string name;
-        private double income;
-        private int vacationDays;
-
-        // Constructor
-
-        public Employee(string name, double income,
-            int vacationDays)
-        {
-            this.name = name;
-            this.income = income;
-            this.vacationDays = vacationDays;
-        }
-
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
-
-        public double Income
-        {
-            get { return income; }
-            set { income = value; }
-        }
-
-        public int VacationDays
-        {
-            get { return vacationDays; }
-            set { vacationDays = value; }
-        }
-
-        public override void Accept(IVisitor visitor)
-        {
-            visitor.Visit(this);
-        }
+        BaggageStatus(flightNo, String.Empty, 0);
     }
 
-    /// <summary>
-    /// The 'ObjectStructure' class
-    /// </summary>
-
-    public class Employees
+    public void BaggageStatus(int flightNo, string from, int carousel)
     {
-        private List<Employee> employees = new List<Employee>();
+        var info = new BaggageInfo(flightNo, from, carousel);
 
-        public void Attach(Employee employee)
+        // Carousel is assigned, so add new info object to list.
+        if (carousel > 0 && !flights.Contains(info))
         {
-            employees.Add(employee);
+            flights.Add(info);
+            foreach (var observer in observers)
+                observer.OnNext(info);
         }
-
-        public void Detach(Employee employee)
+        else if (carousel == 0)
         {
-            employees.Remove(employee);
-        }
-
-        public void Accept(IVisitor visitor)
-        {
-            foreach (Employee employee in employees)
+            // Baggage claim for flight is done
+            var flightsToRemove = new List<BaggageInfo>();
+            foreach (var flight in flights)
             {
-                employee.Accept(visitor);
+                if (info.FlightNumber == flight.FlightNumber)
+                {
+                    flightsToRemove.Add(flight);
+                    foreach (var observer in observers)
+                        observer.OnNext(info);
+                }
             }
+            foreach (var flightToRemove in flightsToRemove)
+                flights.Remove(flightToRemove);
+
+            flightsToRemove.Clear();
+        }
+    }
+
+    public void LastBaggageClaimed()
+    {
+        foreach (var observer in observers)
+            observer.OnCompleted();
+
+        observers.Clear();
+    }
+}
+
+internal class Unsubscriber<BaggageInfo> : IDisposable
+{
+    private List<IObserver<BaggageInfo>> _observers;
+    private IObserver<BaggageInfo> _observer;
+
+    internal Unsubscriber(List<IObserver<BaggageInfo>> observers, IObserver<BaggageInfo> observer)
+    {
+        this._observers = observers;
+        this._observer = observer;
+    }
+
+    public void Dispose()
+    {
+        if (_observers.Contains(_observer))
+            _observers.Remove(_observer);
+    }
+}
+
+public class ArrivalsMonitor : IObserver<BaggageInfo>
+{
+    private string name;
+    private List<string> flightInfos = new List<string>();
+    private IDisposable cancellation;
+    private string fmt = "{0,-20} {1,5}  {2, 3}";
+
+    public ArrivalsMonitor(string name)
+    {
+        if (String.IsNullOrEmpty(name))
+            throw new ArgumentNullException("The observer must be assigned a name.");
+
+        this.name = name;
+    }
+
+    public virtual void Subscribe(BaggageHandler provider)
+    {
+        cancellation = provider.Subscribe(this);
+    }
+
+    public virtual void Unsubscribe()
+    {
+        cancellation.Dispose();
+        flightInfos.Clear();
+    }
+
+    public virtual void OnCompleted()
+    {
+        flightInfos.Clear();
+    }
+
+    // No implementation needed: Method is not called by the BaggageHandler class.
+    public virtual void OnError(Exception e)
+    {
+        // No implementation.
+    }
+
+    // Update information.
+    public virtual void OnNext(BaggageInfo info)
+    {
+        bool updated = false;
+
+        // Flight has unloaded its baggage; remove from the monitor.
+        if (info.Carousel == 0)
+        {
+            var flightsToRemove = new List<string>();
+            string flightNo = String.Format("{0,5}", info.FlightNumber);
+
+            foreach (var flightInfo in flightInfos)
+            {
+                if (flightInfo.Substring(21, 5).Equals(flightNo))
+                {
+                    flightsToRemove.Add(flightInfo);
+                    updated = true;
+                }
+            }
+            foreach (var flightToRemove in flightsToRemove)
+                flightInfos.Remove(flightToRemove);
+
+            flightsToRemove.Clear();
+        }
+        else
+        {
+            // Add flight if it does not exist in the collection.
+            string flightInfo = String.Format(fmt, info.From, info.FlightNumber, info.Carousel);
+            if (!flightInfos.Contains(flightInfo))
+            {
+                flightInfos.Add(flightInfo);
+                updated = true;
+            }
+        }
+        if (updated)
+        {
+            flightInfos.Sort();
+            Console.WriteLine("Arrivals information from {0}", this.name);
+            foreach (var flightInfo in flightInfos)
+                Console.WriteLine(flightInfo);
+
             Console.WriteLine();
         }
     }
+}
 
-    // Three employee types
 
-    public class Clerk : Employee
+public class Example
+{
+    public static void Main()
     {
-        // Constructor
+        BaggageHandler provider = new BaggageHandler();
+        ArrivalsMonitor observer1 = new ArrivalsMonitor("BaggageClaimMonitor1");
+        ArrivalsMonitor observer2 = new ArrivalsMonitor("SecurityExit");
 
-        public Clerk()
-            : base("Kevin", 25000.0, 14)
-        {
-        }
-    }
-
-    public class Director : Employee
-    {
-        // Constructor
-        public Director()
-            : base("Elly", 35000.0, 16)
-        {
-        }
-    }
-
-    public class President : Employee
-    {
-        // Constructor
-        public President()
-            : base("Eric", 45000.0, 21)
-        {
-        }
+        provider.BaggageStatus(712, "Detroit", 3);
+        observer1.Subscribe(provider);
+        provider.BaggageStatus(712, "Kalamazoo", 3);
+        provider.BaggageStatus(400, "New York-Kennedy", 1);
+        provider.BaggageStatus(712, "Detroit", 3);
+        observer2.Subscribe(provider);
+        provider.BaggageStatus(511, "San Francisco", 2);
+        provider.BaggageStatus(712);
+        observer2.Unsubscribe();
+        provider.BaggageStatus(400);
+        provider.LastBaggageClaimed();
     }
 }
