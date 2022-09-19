47cf75e47d6fb909dbb2f0c4916e2bd6fb171c64
Sean Park <whiteibescu@gmail.com>
2022-09-19 23:07:30 +0900

    delegate

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,1 +1,33 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelegateExample : MonoBehaviour
+{
+    Action<int> del;
+    //delegate void NumDelegate(int num);
+    //NumDelegate del;
+
+    private void Start()
+    {
+        del = FirstPlus;
+        del(5);
+
+        del = SecondPlus;
+        del(10);
+    }
+
+
+    void FirstPlus(int num)
+    {
+        int result = num * num;
+        $"FistPlus : {result}".Log();
+    }
+
+    void SecondPlus(int num)
+    {
+        int result = num * num;
+        $"Second Plus : {result}".Log();
+    }
+}
