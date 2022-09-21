488cb1318cf2c24b0265934541b5b50d9532255e
Sean Park <whiteibescu@gmail.com>
2022-09-21 22:50:57 +0900

    Unity Singleton

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,48 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using UnityEngine;
 
-namespace HustarDay08
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
-
-    class Enemy
+    private static T instance = null;
+    public static T Instance
     {
-        public Enemy(string EnemyName)
+        get
         {
-            m_EnemyName = EnemyName;
-            EnemyMgr.StopGameEvent += () => { };
-        }
+            instance = (T)FindObjectOfType(typeof(T));
 
-        private void EnemyMgr_StopGameEvent()
-        {
-            throw new NotImplementedException();
-        }
-        private string m_EnemyName;
-    }
-    delegate void StopGameDelegate();
-
-    class EnemyMgr
-    {
-        public void DiePlayer()
-        {
-            StopGameEvent();
-        }
-        public static event StopGameDelegate StopGameEvent;
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            EnemyMgr mgr = new EnemyMgr();
-            Enemy m1 = new Enemy("적군1");
-            Enemy m2 = new Enemy("적군2");
-            Enemy m3 = new Enemy("적군3");
-            Enemy m4 = new Enemy("적군4");
-            Enemy m5 = new Enemy("적군5");
-            Enemy m6 = new Enemy("적군6");
-            mgr.DiePlayer();
+            if (instance == null)
+            {
+                instance = new GameObject;
+            }
+            return instance;
         }
     }
 }
