ae958b42428861f102786b1a80cc8e49638d11a1
Sean Park <whiteibescu@gmail.com>
2022-09-23 17:48:04 +0900

    practicing

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,20 +1,45 @@
-﻿using UnityEngine;
-
-
-public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using Newtonsoft.Json;
+public class ParseJson : MonoBehaviour
 {
-    private static T instance = null;
-    public static T Instance
+    private string url = "https://jsonplaceholder.typicode.com/comments";
+    private ScrollView scrollView;
+    void Awake()
     {
-        get
-        {
-            instance = (T)FindObjectOfType(typeof(T));
+        scrollView = GetComponent<ScrollView>();
+    }
 
-            if (instance == null)
-            {
-                instance = new GameObject;
-            }
-            return instance;
+    public IEnumerator makeAddressRequest()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(url);
+        yield return request.SendWebRequest();
+
+
+        if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.Log(request.error);
+        }
+        /*Success = 1,
+         ConnectionError = 2,
+         ProtocolError = 3,
+         DataProcessingError = 4*/
+        else
+        {
+            var address = JsonConvert.DeserializeObject<List<Address>>(request.downloadHandler.text);
+            ScrollView.addressList = address;
         }
     }
+}
+
+public class Address
+{
+    public int postid { get; set; }
+    public int id { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string body { get; set; }
 }
