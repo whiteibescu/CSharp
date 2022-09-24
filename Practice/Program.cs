c4def44536b6d1b4d1009515194605fa65a69824
Sean Park <whiteibescu@gmail.com>
2022-09-25 00:57:52 +0900

    Saving Colors in PlayerPrefs

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿// Store single color
+Color color;
+PlayerPrefs.SetString("ColorKey", ColorUtility.ToHtmlStringRGB(color)); // without alpha
+PlayerPrefs.SetString("ColorKey", ColorUtility.ToHtmlStringRGBA(color)); // with alpha
+
+// Load single color
+Color color;
+ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("ColorKey"), out color);
+
+// store color array
+Color[] colors;
+//// fill array with your colors ////
+int numberOfColors = colors.Length;
+for (int i = 0; i < numberOfColors; i++)
+{
+    Color color = colors[i];
+    PlayerPrefs.SetString("ColorKey" + i, ColorUtility.ToHtmlStringRGBA(color)); // with alpha
+}
+
+// load color array
+Color[] colors = new Color[numberOfColors];
+for (int i = 0; i < numberOfColors; i++)
+{
+    ColorUtility.TryParseHtmlString("#" + PlayerPrefs.GetString("ColorKey" + i), out colors[i]);
+}
